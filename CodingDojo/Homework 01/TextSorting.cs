bf6d6fed9103fb7469123736f862db9bf8a8a444
Anawat <anawat@perfenterprise.com>
2018-11-12 18:44:50 +0700

    setup homework01

---
+++ CodingDojo/Homework 01/TextSorting.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_01
+{
+    public class TextSorting : ITextSorting
+    {
+        public string SortByAlphabetical(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CodingDojo/HomeworkXUnit/TextSortingUnitTest.cs
@@ -0,0 +1,22 @@
+using Homework_01;
+using System;
+using Xunit;
+
+namespace HomeworkXUnit
+{
+    public class TextSortingUnitTest
+    {
+        private ITextSorting ITS;
+
+        public TextSortingUnitTest()
+        {
+            ITS = new TextSorting();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
