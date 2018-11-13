be164db8b39059fc008ae70bbfe92a3a9935bd92
Sikarin <so.sikarin@perenterprise.com>
2018-11-13 21:05:47 +0700

    string format

--- Homework 02/Homework.Test/multipliedBy11Test.cs
+++ Homework 02/Homework.Test/multipliedBy11Test.cs
@@ -19,7 +19,7 @@ namespace Homework.Test
         {
             var sut = new MultipliedBy11();
             var result = sut.GetFormattedString("34,67,55,33,12,28");
-            Assert.Equal("[/n/t374,/n/t737,/n/t605,/n/t363,/n/t132,/n/t308/n]", result);
+            Assert.Equal("[\n\t374,\n\t737,\n\t605,\n\t363,\n\t132,\n\t308\n]", result);
         }
     }
 }
