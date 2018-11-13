dfbf429d6128b56a2ad8e3e95ac9b1cfc5f5c810
aphaphon <pui.aphaphon@gmail.com>
2018-11-13 20:10:02 +0700

    Implement Homework01 completed
    Signed-off-by: aphaphon <pui.aphaphon@gmail.com>

---
+++ HomeWork 01/homework01/TextSorting.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static homework01.SortingText;
+
+namespace homework01
+{
+    public class TextSorting : ITextSorting
+    {
+        public string SortByAlphabetical(string text)
+        {
+            var split = text.Split(',');
+            Array.Sort(split);
+            var result = string.Join(",",split);
+            return result;
+        }
+    }
+}
