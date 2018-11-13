9324c7e8790023b230037cfd88d1f84e0de6b661
parada <parada@perfenterprise.com>
2018-11-13 15:26:23 +0700

    Init project for homework01

---
+++ src/DojoDay01/Homework01.Lib/TextSorting.cs
@@ -0,0 +1,13 @@
+﻿using Homework01.Lib.Contract;
+using System;
+
+namespace Homework01.Lib
+{
+    public class TextSorting : ITextSorting
+    {
+        public string SortByAlphabetical(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
