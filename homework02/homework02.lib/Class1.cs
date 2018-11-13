5b27747e29157a27062914fe3b700898ba120c2e
woraput <woraput@kkumail.com>
2018-11-14 00:05:00 +0700

    setup homework02 and add reference both .test and .console

---
+++ homework02/homework02.lib/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace homework02.lib
+{
+    public class Class1
+    {
+    }
+}
---
+++ homework02/homework02.test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace homework02.test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
