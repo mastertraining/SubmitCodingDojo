c4dbef663e18c761b22f6b72c82b105122ea8616
guntza22 <guntza22@hotmail.com>
2018-11-13 17:35:33 +0700

    done HomeWork1 and HomeWork2

---
+++ Homework 01/HomeWork1.console/SortStringProgram.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using HomeWork1.classlib;
+using System.Text;
+using System.Linq;
+
+namespace HomeWork1.console
+{
+    class SortStringProgram
+    {
+        static void Main(string[] args)
+        {
+            var sb = new StringBuilder();
+            var stringSort = new StringSort();
+            Console.Write("Enter String : ");
+            string inputString = Console.ReadLine();
+            // var text = sb.Append("without,hello,bag,world").ToString();
+            string text = sb.Append(inputString).ToString();
+            var resultSort = stringSort.SortByAlphabetical(text);
+            Console.Write(resultSort);
+        }
+    }
+}
---
+++ Homework 02/HomeWork2.classlib/SortNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace HomeWork2.classlib
+{
+    public class SortNumber : ITextMultiplier
+    {
+        public string GetFormattedString(string text)
+        {
+            var sb = new StringBuilder();
+            var subString = text.Split(',');
+            var numberList = new List<string>();
+            foreach (var number in subString)
+            {
+                int result;
+                result = int.Parse(number) * 11;
+                numberList.Add(result.ToString());
+            }
+
+            sb.AppendLine("[");
+
+            for (int i = 0 ; i < numberList.Count ; i++) {
+        
+                if(i == numberList.Count-1) {
+                    sb.Append("\t").AppendLine(numberList[i]).Append("]");
+                }
+                else {
+                     sb.Append("\t").Append(numberList[i]).AppendLine(",");
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
