a1910653b00077b907c7f018a3c02ef1b5b20bf6
Sikarin <so.sikarin@perenterprise.com>
2018-11-13 20:59:11 +0700

    imprement ITextMultiplier

--- Homework 02/Homework.Lib/Homework.cs
+++ Homework 02/Homework.Lib/Homework.cs
@@ -2,7 +2,21 @@
 
 namespace Homework.Lib
 {
-    public class MultipliedBy11
+    public class MultipliedBy11 : ITextMultiplier
     {
+        public string GetFormattedString(string text)
+        {
+            var split = text.Split(',');
+            for (int i = 0; i < split.Length; i++)
+            {
+                var convert = int.Parse(split[i]);
+                split[i] = (convert * 11).ToString();
+            }
+
+            var result = string.Format("[/n/t{0},/n/t{1},/n/t{2},/n/t{3},/n/t{4},/n/t{5}/n]",
+            split[0],split[1],split[2],split[3],split[4],split[5]);
+            return result;
+        }
+
     }
 }
--- Homework 02/Homework.Test/multipliedBy11Test.cs
+++ Homework 02/Homework.Test/multipliedBy11Test.cs
@@ -1,14 +1,25 @@
 using System;
+using Homework.Lib;
 using Xunit;
 
 namespace Homework.Test
 {
     public class MultipliedBy11Test
     {
-        [Fact]
-        public void Test1()
-        {
+        // [Fact]
+        // public void MultipliedBy11()
+        // {
+        //     var sut = new MultipliedBy11();
+        //     var result = sut.X11("34,67,55,33,12,28");
+        //     Assert.Equal("374,737,605,363,132,308", result);
+        // }
 
+        [Fact]
+        public void StringFormat()
+        {
+            var sut = new MultipliedBy11();
+            var result = sut.GetFormattedString("34,67,55,33,12,28");
+            Assert.Equal("[/n/t374,/n/t737,/n/t605,/n/t363,/n/t132,/n/t308/n]", result);
         }
     }
 }
