855d5da1ac7b20e881b5053b1f59f6dbe498f1ef
apiched1 <apiched_oh@hotmail.com>
2018-11-13 18:19:54 +0700

    Create Project Homework1

---
+++ Homework01/Homework1.classlib/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Homework1.classlib
+{
+    public class Class1
+    {
+    }
+}
---
+++ Homework01/Homework1.console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Homework1.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
