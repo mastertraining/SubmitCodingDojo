3077864e637611c9eae5b3a36525b1f9f49ec75d
Anawat <anawat@perfenterprise.com>
2018-11-14 18:48:20 +0700

    HW03: Test interface

---
+++ CodingDojo/HomeworkXUnit/Homework03UnitTest.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using Homework_03;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace HomeworkXUnit
+{
+    public class Homework03UnitTest
+    {
+        private IHomework03 IHW;
+
+        public Homework03UnitTest()
+        {
+            IHW = new Homework03();
+        }
+
+        [Theory]
+        [MemberData(nameof(GetCapitalizedTextCases))]
+        public void CapitalizedTextShouldWork(IEnumerable<string> text, IEnumerable<string> expected)
+        {
+            var result = IHW.CapitalizedText(text);
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        public static IEnumerable<object[]> GetCapitalizedTextCases = new List<object[]>
+        {
+            new object[] {
+                new string[] { "Hello world", "Practice makes perfect" } ,
+                new string[] { "HELLO WORLD", "PRACTICE MAKES PERFECT"
+                }
+            }
+        };
+    }
+}
