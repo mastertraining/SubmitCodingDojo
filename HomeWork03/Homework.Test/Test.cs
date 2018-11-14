95a04d8d460ea67087f0c833259adeb74f0168b2
Kritsana <tragoolphrom@gmail.com>
2018-11-14 19:58:46 +0700

    HomeWork04 Finish

--- HomeWork03/Homework.Test/Test.cs
+++ HomeWork03/Homework.Test/Test.cs
@@ -10,11 +10,11 @@ namespace Homework.Test
         [Theory]
         [InlineData(new string[] { "Hello world", "Practice makes perfect" }, new string[] { "HELLO WORLD", "PRACTICE MAKES PERFECT" })]
         [InlineData(new string[] { "Hello world", "Practice makes perfect", "Testing" }, new string[] { "HELLO WORLD", "PRACTICE MAKES PERFECT", "TESTING" })]
-        public void TestSortByAlphabetical(string[] text, string[] expectNumber)
+        public void TestSortByAlphabetical(string[] text, string[] expectText)
         {
             var sut = new Homework03();
             var result = sut.CapitalizedText(text);
-            Assert.Equal(expectNumber, result);
+            Assert.Equal(expectText, result);
         }
     }
 }
