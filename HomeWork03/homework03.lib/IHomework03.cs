db52561385a8b717e474d4c799af9753d676ddbb
Earn <earndarkness@perfenterprise.com>
2018-11-14 19:38:10 +0700

    Implement homework03
    Signed-off-by: Earn <earndarkness@perfenterprise.com>

---
+++ HomeWork03/homework03.lib/IHomework03.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+namespace homework03.lib
+{
+    public interface IHomework03
+    {
+        IEnumerable<string> CapitalizedText(IEnumerable<string> text);
+    }
+}
