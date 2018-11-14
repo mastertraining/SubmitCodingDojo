4215a3fbd3e0d07880092f2e620375d13e7bbcb6
Anawat <anawat@perfenterprise.com>
2018-11-14 18:39:01 +0700

    setup HW3 & HW4

---
+++ CodingDojo/Homework 03/Homework03.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework_03
+{
+    public class Homework03 : IHomework03
+    {
+        public IEnumerable<string> CapitalizedText(IEnumerable<string> text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CodingDojo/Homework 04/Homework04.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework_04
+{
+    public class Homework04 : IHomework04
+    {
+        public string GetDivisibleBy5Text(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
