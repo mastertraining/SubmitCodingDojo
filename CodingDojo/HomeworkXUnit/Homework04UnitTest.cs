2f230317fcca97503c18afacc6cc2ceaa9367ad8
Anawat <anawat@perfenterprise.com>
2018-11-14 18:52:46 +0700

    HW04: Test interface

---
+++ CodingDojo/HomeworkXUnit/Homework04UnitTest.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using Homework_04;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace HomeworkXUnit
+{
+    public class Homework04UnitTest
+    {
+        private IHomework04 IHW;
+
+        public Homework04UnitTest()
+        {
+            IHW = new Homework04();
+        }
+
+        [Theory]
+        [InlineData("0100,0011,1010,1001", "1010")]
+        public void GetDivisibleBy5TextShouldWork(string text, string expected)
+        {
+            var result = IHW.GetDivisibleBy5Text(text);
+            result.Should().Be(expected);
+        }
+    }
+}
