9fbbc35c2df3710b7fb456a3445df16d37a25cf9
Anawat <anawat@perfenterprise.com>
2018-11-14 18:39:01 +0700

    setup HW3 & HW4

---
+++ CodingDojo/Homework 03/IHomework03.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework_03
+{
+    public interface IHomework03
+    {
+        IEnumerable<string> CapitalizedText(IEnumerable<string> text);
+    }
+}
