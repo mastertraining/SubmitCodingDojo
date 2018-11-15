ba41d53ecfbfaaf5386fdeffe844aa63f261afb8
woraput <woraput@kkumail.com>
2018-11-15 23:55:46 +0700

    setup hw4

---
+++ homework04/hw4ClassLib/Homework04.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hw4ClassLib
+{
+    public class Homework04 : IHomework04
+    {
+        public string GetDivisibleBy5Text(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ homework04/hw4Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace hw4Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
