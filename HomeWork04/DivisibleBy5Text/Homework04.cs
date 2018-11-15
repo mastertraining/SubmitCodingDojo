004d955f897f3264e25b2f0affc671ffe0efddf2
Perawatt <pekung4433@perfenterprise.com>
2018-11-15 17:09:03 +0700

    completed Homework04

---
+++ HomeWork04/DivisibleBy5Text/Homework04.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace DivisibleBy5Text
+{
+    public class Homework04 : IHomework04
+    {
+        public string GetDivisibleBy5Text(string text)
+        {
+            var numberList = text.Split(',').Select(it => Convert.ToInt32(it,2)).ToList();
+            var numberDivideByFive = numberList.Where(it => it % 5 == 0).Select(it => Convert.ToString(it,2)).ToList();
+            return String.Join(",", numberDivideByFive);
+        }
+    }
+}
---
+++ HomeWork04/DivisibleBy5Text/IHomework04.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DivisibleBy5Text
+{
+    public interface IHomework04
+    {
+        string GetDivisibleBy5Text(string text);
+    }
+}
