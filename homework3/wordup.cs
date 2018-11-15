d761a3a9f8df5a4bd9770fb3ddbe8e2b82ae2c35
tatae <tachapong999.gmail.com>
2018-11-15 22:40:56 +0700

    complete homework3 start homework4

---
+++ homework3/wordup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace homework3 
+{
+    public class workup : IHomework03
+    {
+        public IEnumerable<string> CapitalizedText(IEnumerable<string> text)
+        {
+            text = new string []{};
+            var word = text.Select(it=> it.ToUpper());
+            return word;
+           
+           
+        }
+    }
+
+}
+
---
+++ testhomework3/homeworktest3.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using homework3;
+using Xunit;
+
+namespace testhomework3
+{
+
+    public class homeworktest3
+    {
+        [Theory]
+        [InlineData()]
+        public void Test1(IEnumerable<string> text, IEnumerable<string> excepted)
+        {
+            var upper = new workup();
+            var result = upper.CapitalizedText(text);
+            Assert.Equal(excepted, result);
+
+        }
+    }
+}
