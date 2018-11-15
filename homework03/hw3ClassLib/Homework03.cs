1448076a4694c739daf59e1bcd06edcfeff9e1dd
woraput <woraput@kkumail.com>
2018-11-15 11:14:08 +0700

    serup hm3

---
+++ homework03/hw3ClassLib/Homework03.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace hw3ClassLib
+{
+    public class Homework03 : IHomework03
+    {
+        public IEnumerable<string> CapitalizedText(IEnumerable<string> text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ homework03/hw3Test/TestHomework03.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace hw3Test
+{
+    public class TestHomework03
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
