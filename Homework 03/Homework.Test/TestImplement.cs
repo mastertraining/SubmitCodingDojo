aaf1a181229289c324bfa4001ae885a5826df2f6
Sikarin <so.sikarin@perenterprise.com>
2018-11-15 21:41:27 +0700

    implement binary

--- Homework 03/Homework.Test/TestImplement.cs
+++ Homework 03/Homework.Test/TestImplement.cs
@@ -7,7 +7,10 @@ namespace Homework.Test {
     public class TestImplement {
         [Fact]
         public void RepaceToUper () {
-            
+            var sut = new Implement();
+            string[] input = new string[]{"SorrY","sORRy"};
+            var result = sut.CapitalizedText(input);
+            Assert.Equal(new string[]{"SORRY","SORRY"},result);
         }
     }
 }
