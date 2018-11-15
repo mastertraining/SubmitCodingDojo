a162a14e6101f0cdb31ee368c021f28e61302409
apiched1 <apiched_oh@hotmail.com>
2018-11-15 17:10:06 +0700

    Create Homework3 and Finish Homework3

---
+++ Homework03/Homework03.console/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Homework03.classlib;
+
+namespace Homework03.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> text = new List<string>();
+
+            System.Console.WriteLine("Input String for Upper :");
+            int String_Line = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < String_Line; i++)
+            {
+                text.Add(Console.ReadLine());
+            }
+            var Text_Upper = new StringToUpper();
+            var result = Text_Upper.GetFormattedString(text);
+            
+            foreach (var item in result)
+            {
+                System.Console.WriteLine(item);
+            }   
+           
+            
+
+            
+            // var String_sum = new StringSort();
+            // string result = String_sum.ToUpper();
+            // Console.WriteLine("Hello World!");
+        }
+    }
+}
