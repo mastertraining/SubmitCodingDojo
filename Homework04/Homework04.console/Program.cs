7a2ed952d28a526ba15b363ccea83c1636592dd0
apiched1 <apiched_oh@hotmail.com>
2018-11-15 17:37:39 +0700

    Create Homework04

---
+++ Homework04/Homework04.console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Homework04.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
