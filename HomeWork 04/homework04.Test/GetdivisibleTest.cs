bce664a7fbf62a24616a27fa16538f4239f74519
aphaphon <pui.aphaphon@gmail.com>
2018-11-16 10:43:18 +0700

    Implement Divisible Completed

--- HomeWork 04/homework04.Test/GetdivisibleTest.cs
+++ HomeWork 04/homework04.Test/GetdivisibleTest.cs
@@ -5,10 +5,13 @@ namespace homework04.Test
 {
     public class GetdivisibleTest
     {
-        [Fact]
-        public void Test1()
-        {
-
+        [Theory]
+        [InlineData("0100,0011,1010,1001","1010")]
+        public void GetDivisibleBy5(string inputString, string expected)
+        { 
+            var sut = new GetdivisibleLogic();
+            var result = sut.GetDivisibleBy5Text(inputString);
+            Assert.Equal(expected,result);
         }
     }
 }
