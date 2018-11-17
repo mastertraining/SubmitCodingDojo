5ce363cfa52898f7737a5f0a97312bbd208ce0e0
rn0z <manutsawee113@gmail.com>
2018-11-17 22:04:30 +0700

    Pause homework5.

--- HomeWork5/Program.cs
+++ HomeWork5/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-namespace HomeWork5
+namespace Homework5
 {
-    class Program
+    class Program: IHomework05
     {
-        static void Main(string[] args)
+        Dictionary<char, int> g = new Dictionary<char, int>
         {
-            Console.WriteLine("Hello World!");
+            {'1', 1}, {'2', 1},
+            {'3', 1}, {'4', 1},
+            {'5', 1}, {'6', 1},
+            {'7', 1}, {'8', 1},
+            {'9', 1}, {'A', 1},
+        };
+        StringBuilder LEDscreen = new StringBuilder();
+        static void Main()
+        {
+            Program prm = new Program();
+            Console.WriteLine(prm.DisplayLEDOnScreen("1"));
+        }
+        
+        static StringBuilder ScreenBuilder() 
+        {
+            StringBuilder sb = new StringBuilder("");
+
+            return sb;
+        }
+        public string DisplayLEDOnScreen(string ledNo = "")
+        {
+            this.LEDscreen = ScreenBuilder();
+            char idx = (ledNo.Length > 0)? ledNo[0] : ' ';
+
+
+
+            return LEDscreen.ToString();
         }
     }
 }
