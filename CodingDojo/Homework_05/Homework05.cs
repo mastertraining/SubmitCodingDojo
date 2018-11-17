017d0de6829f325785ea9a282f5c7ec1aa5d0516
Anawat <anawat@perfenterprise.com>
2018-11-18 00:03:13 +0700

    HW05: Implement Homework05

--- CodingDojo/Homework_05/Homework05.cs
+++ CodingDojo/Homework_05/Homework05.cs
@@ -7,14 +7,33 @@ namespace Homework_05
 {
     public class Homework05 : IHomework05
     {
-        private IEnumerable<string> led;
+        private IList<string> LedIndex;
+        private IList<string> ledStatus;
+        private readonly int ledCount = 10;
+        private readonly string ledOnSign = "[!]";
+        private readonly string ledOffSign = "[ ]";
+        private readonly string ledStatusWriteSpace = " ";
+        private readonly string ledIndexWriteSpace = "   ";
+        private IDictionary<int, string> ledIndexName = new Dictionary<int, string>();
+        
         public Homework05() {
-            led = Enumerable.Repeat("[ ]", 10);
+            RegisterIndexName();
+            ledStatus = Enumerable.Repeat(ledOffSign, ledCount).ToList();
+            LedIndex = Enumerable.Range(1, ledCount)
+                .Select(it => ledIndexName.TryGetValue(it, out var value) ? value : it.ToString())
+                .ToList();
         }
+
+        private void RegisterIndexName() => ledIndexName.Add(10, "A");
 
         public string DisplayLEDOnScreen(string ledNo)
         {
-            throw new NotImplementedException();
+            var index = LedIndex.IndexOf(ledNo);
+            ledStatus[index] = ledStatus[index] == ledOffSign ? ledOnSign : ledOffSign;
+
+            var eachLedStatus = string.Join(ledStatusWriteSpace, ledStatus);
+            var eachLedIndex = string.Join(ledIndexWriteSpace, LedIndex);
+            return $"{eachLedStatus}{Environment.NewLine} {eachLedIndex}";    
         }
     }
 }
