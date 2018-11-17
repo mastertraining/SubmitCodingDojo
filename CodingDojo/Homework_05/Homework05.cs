bfcb994901a843cce46a209f3fc3eee31bc4aff0
Anawat <anawat@perfenterprise.com>
2018-11-17 23:14:40 +0700

    HW05: Test interface and prepare Homework05 code

--- CodingDojo/Homework_05/Homework05.cs
+++ CodingDojo/Homework_05/Homework05.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Homework_05
 {
     public class Homework05 : IHomework05
     {
+        private IEnumerable<string> led;
+        public Homework05() {
+            led = Enumerable.Repeat("[ ]", 10);
+        }
+
         public string DisplayLEDOnScreen(string ledNo)
         {
             throw new NotImplementedException();
