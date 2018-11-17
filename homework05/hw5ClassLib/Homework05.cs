c8aab8fc7d44661ed5b391af76ff30ba3012a586
woraput <woraput@kkumail.com>
2018-11-17 18:46:49 +0700

    setup hw5

---
+++ homework05/hw5ClassLib/Homework05.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hw5ClassLib
+{
+    public class Homework05 : IHomework05
+    {
+        public string DisplayLEDOnScreen(string ledNo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
