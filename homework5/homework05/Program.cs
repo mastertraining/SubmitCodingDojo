8a979dda7a3a9fd7b207fe802c7ce845ca508f3b
tatae <tachapong999.gmail.com>
2018-11-19 01:48:10 +0700

    show result input swict on

---
+++ homework5/homework05/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace homework05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("[ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ]");
+            Console.WriteLine(" 1   2   3   4   5   6   7   8   9   A");
+            string input = Console.ReadLine();
+           if (input == "1") 
+           {
+               System.Console.WriteLine("[!] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ]");
+               Console.WriteLine(" 1   2   3   4   5   6   7   8   9   A");
+           }
+            if (input == "2") 
+           {
+               System.Console.WriteLine("[ ] [!] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ]");
+               Console.WriteLine(" 1   2   3   4   5   6   7   8   9   A");
+           }
+             if (input == "3") 
+           {
+               System.Console.WriteLine("[ ] [ ] [!] [ ] [ ] [ ] [ ] [ ] [ ] [ ]");
+               Console.WriteLine(" 1   2   3   4   5   6   7   8   9   A");
+           }
+             if (input == "4") 
+           {
+               System.Console.WriteLine("[ ] [ ] [ ] [!] [ ] [ ] [ ] [ ] [ ] [ ]");
+               Console.WriteLine(" 1   2   3   4   5   6   7   8   9   A");
+           }
+             if (input == "5") 
+           {
+               System.Console.WriteLine("[ ] [ ] [ ] [ ] [!] [ ] [ ] [ ] [ ] [ ]");
+               Console.WriteLine(" 1   2   3   4   5   6   7   8   9   A");
+           }
+             if (input == "6") 
+           {
+               System.Console.WriteLine("[ ] [ ] [ ] [ ] [ ] [!] [ ] [ ] [ ] [ ]");
+               Console.WriteLine(" 1   2   3   4   5   6   7   8   9   A");
+           }
+             if (input == "7") 
+           {
+               System.Console.WriteLine("[ ] [ ] [ ] [ ] [ ] [ ] [!] [ ] [ ] [ ]");
+               Console.WriteLine(" 1   2   3   4   5   6   7   8   9   A");
+           }
+             if (input == "8") 
+           {
+               System.Console.WriteLine("[ ] [ ] [ ] [ ] [ ] [ ] [ ] [!] [ ] [ ]");
+               Console.WriteLine(" 1   2   3   4   5   6   7   8   9   A");
+           }
+             if (input == "9") 
+           {
+               System.Console.WriteLine("[ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [!] [ ]");
+               Console.WriteLine(" 1   2   3   4   5   6   7   8   9   A");
+           }
+             if (input == "a" || input == "A") 
+           {
+               System.Console.WriteLine("[ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [!]");
+               Console.WriteLine(" 1   2   3   4   5   6   7   8   9   A");
+           }
+        }
+    }
+}
