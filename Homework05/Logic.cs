20886ee9c72cd112652894e53aadd4e5fd35d55b
powerofwind <powerofwind10@gmail.com>
2018-11-18 23:44:10 +0700

    set up project

---
+++ Homework05/Logic.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Homework05
+{
+    class Logic : IHomework05
+    {
+        public string DisplayLEDOnScreen(string ledNo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
