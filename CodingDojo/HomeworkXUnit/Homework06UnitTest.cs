0229c9066dda2017192a804cd09aae85bcb562d8
Anawat <anawat@perfenterprise.com>
2018-11-19 20:34:43 +0700

    HW06: Implement Homework06 and fix error of Homework05 (no index)

--- CodingDojo/HomeworkXUnit/Homework06UnitTest.cs
+++ CodingDojo/HomeworkXUnit/Homework06UnitTest.cs
@@ -2,6 +2,7 @@
 using Homework_06;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using Xunit;
 
 namespace HomeworkXUnit
@@ -19,28 +20,75 @@ namespace HomeworkXUnit
         [MemberData(nameof(DisplayLEDOnScreenCase))]
         public void DisplayLEDOnScreenShouldWork(string[] ledNo, string expected)
         {
-            var result = string.Empty;
-            foreach (var No in ledNo)
+            try
             {
-                result = IHW.DisplayLEDOnScreen(No);
+                var result = string.Empty;
+                foreach (var No in ledNo)
+                {
+                    result = IHW.DisplayLEDOnScreen(No);
+                }
+                result.Should().Be(expected);
             }
-            result.Should().Be(expected);
+            finally
+            {
+                DeleteFiles();
+            }
         }
 
         [Theory]
         [MemberData(nameof(DisplayLEDOnScreenCase))]
         public void SaveCurrentStateShouldWork(string[] ledNo, string expected)
         {
-            var currentState = string.Empty;
-            foreach (var No in ledNo)
+            try
             {
-                currentState = IHW.DisplayLEDOnScreen(No);
+                IHW.LoadState();
+                var currentState = string.Empty;
+                foreach (var No in ledNo)
+                {
+                    currentState = IHW.DisplayLEDOnScreen(No);
+                }
+                IHW.SaveCurrentState();
+                currentState.Should().Be(expected);
+                var HW06 = new Homework06();
+                var result = HW06.LoadState();
+                result.Should().Be(expected);
             }
-            IHW.SaveCurrentState();
-            currentState.Should().Be(expected);
-            var HW06 = new Homework06();
-            var result = HW06.LoadState();
-            result.Should().Be(expected);
+            finally
+            {
+                DeleteFiles();
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(ConfigurationsCase))]
+        public void SetAppConfigurationsShouldWork(string[] ledNo, string onSymbol, string offSymbol, int spacing, string expected)
+        {
+            try
+            {
+                IHW.SetAppConfigurations(onSymbol, offSymbol, spacing);
+                var result = string.Empty;
+                foreach (var No in ledNo)
+                {
+                    result = IHW.DisplayLEDOnScreen(No);
+                }
+                result.Should().Be(expected);
+                IHW.SaveCurrentState();
+                var HW06 = new Homework06();
+                var anotherResult = HW06.LoadState();
+                anotherResult.Should().Be(expected);
+            }
+            finally
+            {
+                DeleteFiles();
+            }
+        }
+
+        private void DeleteFiles()
+        {
+            var configPath = @"config.yaml";
+            var saveStatePath = @"saveLedOnIndex.txt";
+            File.Delete(configPath);
+            File.Delete(saveStatePath);
         }
 
         public static IEnumerable<object[]> DisplayLEDOnScreenCase = new List<object[]>
