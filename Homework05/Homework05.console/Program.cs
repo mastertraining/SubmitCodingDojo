a07763d92f8581d81dabdf11246198b8307e8fd6
apiched1 <apiched_oh@hotmail.com>
2018-11-19 16:48:28 +0700

    Finish Homework5

--- Homework05/Homework05.console/Program.cs
+++ Homework05/Homework05.console/Program.cs
@@ -8,52 +8,17 @@ namespace Homework05.console
     {
         static void Main(string[] args)
         {
-            Dictionary<String, List<string>> dic = new Dictionary<string, List<string>>();
-
-            List<string> li1 = new List<string>();
-            li1.Add("[ ]");
-            li1.Add("[ ]");
-            li1.Add("[ ]");
-            li1.Add("[ ]");
-            li1.Add("[ ]");
-            li1.Add("[ ]");
-            li1.Add("[ ]");
-            li1.Add("[ ]");
-            li1.Add("[ ]");
-            li1.Add("[ ]");
-
-            List<string> li2 = new List<string>();
-            li2.Add(" ");
-            li2.Add(" 1 ");
-            li2.Add(" 2 ");
-            li2.Add(" 3 ");
-            li2.Add(" 4 ");
-            li2.Add(" 5 ");
-            li2.Add(" 6 ");
-            li2.Add(" 7 ");
-            li2.Add(" 8 ");
-            li2.Add(" 9 ");
-            li2.Add(" A ");
-
-            dic["1"] = li1;
-            dic["2"] = li2;
-
-
-            foreach (string key in dic.Keys)
+            var led = new LED();
+            led.defaultLight();
+            var numberSlot = "";
+            do
             {
-                foreach (string val in dic[key])
-                {
-                    Console.Write(val);
-                    if (val == " ")
-                    {
-                        System.Console.WriteLine("");
-                    }
-                    if (true)
-                    {
-                    
-                    }
-                }
-            }
+                var listLED = led.DisplayLEDOnScreen(numberSlot);
+                System.Console.WriteLine(listLED);
+
+                System.Console.Write("Please choose LED to turn On/Off: ");
+                numberSlot = Console.ReadLine();
+            } while (true);
         }
     }
 }
