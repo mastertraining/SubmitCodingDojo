df4af1f3c9ff6df035098d165deb04b9fc622812
Perawatt <pekung4433@perfenterprise.com>
2018-11-19 14:44:40 +0700

    Send HomeWork05

---
+++ HomeWork05/HomeWork05/HomeWork05.console/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HomeWork05.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var svc = new Homework05();
+            Console.WriteLine(svc.DisplayLEDOnScreen(""));
+            while (true)
+            {
+                Console.Write("Please choose LED to turn On/Off: ");
+                var led = svc.DisplayLEDOnScreen(Console.ReadLine());
+                Console.WriteLine(led);
+            }
+        }
+    }
+}
