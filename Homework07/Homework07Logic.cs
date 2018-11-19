a093754813e075aa51e8f838b01591d8c5a35e3e
powerofwind <powerofwind10@gmail.com>
2018-11-19 23:12:43 +0700

    homework07 completed

--- Homework07/Homework07Logic.cs
+++ Homework07/Homework07Logic.cs
@@ -1,7 +1,18 @@
+using System;
+using System.Linq;
+
 namespace Homework07
 {
-    public class Homework07Logic
+    public class Homework07Logic : IHomework07
     {
-        
+        public string RemoveAndSortTextByAlphabetical(string text)
+        {
+            var split = text.Split(' ');
+            var sorting = split.OrderBy(it => it);
+            var result = sorting.Distinct();
+            var joined = string.Join(" ", result);
+
+            return joined;
+        }
     }
 }
---
+++ Homework07/ProgramHomework07.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Homework07
+{
+    class ProgramHomework07
+    {
+        static void Main(string[] args)
+        {
+           System.Console.WriteLine("Please enter your word"); 
+           var input = System.Console.ReadLine();
+
+           var a = new Homework07Logic();
+           var result = a.RemoveAndSortTextByAlphabetical(input);
+           
+           System.Console.WriteLine(result);
+        }
+    }
+}
