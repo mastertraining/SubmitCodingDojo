50b6550047bf1ff01a2e24b6cd1bc85a6a8e7141
powerofwind <powerofwind10@gmail.com>
2018-11-19 23:47:25 +0700

    reflector code completed

--- Homework07/Homework07Logic.cs
+++ Homework07/Homework07Logic.cs
@@ -7,12 +7,8 @@ namespace Homework07
     {
         public string RemoveAndSortTextByAlphabetical(string text)
         {
-            // var split = text.Split(' ');
-            // var sorting = split.OrderBy(it => it);
-            // var result = sorting.Distinct();
-            // var joined = string.Join(" ", result);
             var result = text.Split(' ').OrderBy(it => it).Distinct();
-            return string.Join(" ", text);
+            return string.Join(" ", result);
         }
     }
 }
