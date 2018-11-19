36af8866a50bfb125c21ad25971e2da905ba2eb0
powerofwind <powerofwind10@gmail.com>
2018-11-19 23:36:52 +0700

    reflacter code

--- Homework07/Homework07Logic.cs
+++ Homework07/Homework07Logic.cs
@@ -7,12 +7,12 @@ namespace Homework07
     {
         public string RemoveAndSortTextByAlphabetical(string text)
         {
-            var split = text.Split(' ');
-            var sorting = split.OrderBy(it => it);
-            var result = sorting.Distinct();
-            var joined = string.Join(" ", result);
-
-            return joined;
+            // var split = text.Split(' ');
+            // var sorting = split.OrderBy(it => it);
+            // var result = sorting.Distinct();
+            // var joined = string.Join(" ", result);
+            var result = text.Split(' ').OrderBy(it => it).Distinct();
+            return string.Join(" ", text);
         }
     }
 }
