15d900c2cdcbb5a5dbfd354e9febd4011985b72e
woraput <woraput@kkumail.com>
2018-11-19 13:04:41 +0700

    pass case test

--- homework05/hw5Test/TestHomework05.cs
+++ homework05/hw5Test/TestHomework05.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using hw5ClassLib;
 using Xunit;
 
@@ -7,18 +8,24 @@ namespace hw5Test
     public class TestHomework05
     {
         [Theory]
-        [InlineData("1", "[!] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ] \n 1  2  3  4  5  6  7  8  9  A ")]
-        // [InlineData("2", "[ ][!][ ][ ][ ][ ][ ][ ][ ][ ]\r\n 1  2  3  4  5  6  7  8  9  A ")]
-        // [InlineData("3", "[ ][ ][!][ ][ ][ ][ ][ ][ ][ ]\r\n 1  2  3  4  5  6  7  8  9  A ")]
-        // [InlineData("4", "[ ][ ][ ][!][ ][ ][ ][ ][ ][ ]\r\n 1  2  3  4  5  6  7  8  9  A ")]
-        // [InlineData("5", "[ ][ ][ ][ ][!][ ][ ][ ][ ][ ]\r\n 1  2  3  4  5  6  7  8  9  A ")]
-        // [InlineData("6", "[ ][ ][ ][ ][ ][!][ ][ ][ ][ ]\r\n 1  2  3  4  5  6  7  8  9  A ")]
-        // [InlineData("7", "[ ][ ][ ][ ][ ][ ][!][ ][ ][ ]\r\n 1  2  3  4  5  6  7  8  9  A ")]
+        [MemberData(nameof(Data))]
         public void Test(string ledNo, string expected)
         {
             var ledControl = new Homework05();
             var result = ledControl.DisplayLEDOnScreen(ledNo);
             Assert.Equal(expected, result);
         }
+
+        public static IEnumerable<object[]> Data =>
+        new List<object[]>
+        {
+            new object[] {"1","[!] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ]\r\n 1   2   3   4   5   6   7   8   9   A"},
+            new object[] {"2","[ ] [!] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ]\r\n 1   2   3   4   5   6   7   8   9   A"},
+            new object[] {"3","[ ] [ ] [!] [ ] [ ] [ ] [ ] [ ] [ ] [ ]\r\n 1   2   3   4   5   6   7   8   9   A"},
+            new object[] {"4","[ ] [ ] [ ] [!] [ ] [ ] [ ] [ ] [ ] [ ]\r\n 1   2   3   4   5   6   7   8   9   A"},
+            new object[] {"5","[ ] [ ] [ ] [ ] [!] [ ] [ ] [ ] [ ] [ ]\r\n 1   2   3   4   5   6   7   8   9   A"},
+            new object[] {"6","[ ] [ ] [ ] [ ] [ ] [!] [ ] [ ] [ ] [ ]\r\n 1   2   3   4   5   6   7   8   9   A"},
+
+        };
     }
 }
