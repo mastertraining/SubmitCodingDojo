0f21c4b6abb42f69e968c785ddb933f397483210
sirisak <sirisak022602020sunon@gmail.com>
2018-11-19 15:33:18 +0700

    add Homework05

---
+++ Homework05/Homework_classlib/Homework05.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework_classlib
+{
+    public class Homework05 : IHomework05
+    {
+        public List<string> listLED { get; set; }
+        public List<string> listNoLED { get; set; }
+        public string DisplayLEDOnScreen(string ledNo)
+        {
+            ledNo = ledNo.ToUpper();
+    
+            for (int i = 0; i < listNoLED.Count; i++)
+            {
+                if (ledNo == listNoLED[i])
+                {
+                    if (listLED[i] == "[ ]")
+                    {
+                        listLED[i] = "[!]";
+                    }
+                    else if(listLED[i] == "[!]")
+                    {
+                        listLED[i] = "[ ]";
+                    }
+                }
+            }
+
+            var newString = new StringBuilder();
+            var joinLED = String.Join(" ", listLED);
+            var joinNoLED = String.Join("   ", listNoLED);
+            newString.AppendLine(joinLED).Append(" ").Append(joinNoLED);
+
+            return newString.ToString();
+        }
+    }
+}
