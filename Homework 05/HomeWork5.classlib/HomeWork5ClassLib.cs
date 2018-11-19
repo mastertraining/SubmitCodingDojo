cebfe0ccd3a01ede14ab22e3d48bfa1b96f3d1f6
guntza22 <guntza22@hotmail.com>
2018-11-19 09:55:02 +0700

    Done HomeWork 5

--- Homework 05/HomeWork5.classlib/HomeWork5ClassLib.cs
+++ Homework 05/HomeWork5.classlib/HomeWork5ClassLib.cs
@@ -11,48 +11,39 @@ namespace HomeWork5.classlib
         {
             var listNoLed = new List<string>(new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "A" });
             var listStatusLed = new List<bool>(new bool[] { false, false, false, false, false, false, false, false, false, false });
-            string noLed = ledNo.ToUpper();
+            var noLed = ledNo.ToUpper();
             var sb = new StringBuilder();
             while (true)
             {
                 // for (int i = 0; i < 2; i++)
                 // {
-                    for (int j = 0; j < listNoLed.Count; j++)
+                for (int j = 0; j < listNoLed.Count; j++)
+                {
+                    // if (i == 0)
+                    // {
+                    if (noLed == listNoLed[j])
                     {
-                        // if (i == 0)
-                        // {
-                            if (noLed == listNoLed[j])
-                            {
-                                if (listStatusLed[j] == false)
-                                {
-                                    listStatusLed[j] = true;
-                                }
-                                else
-                                {
-                                    listStatusLed[j] = false;
-                                }
-                            }
-
-                            if (listStatusLed[j])
-                            {
-                                sb.Append("[!]\t");
-                                // Console.Write("[!]\t");
-                            }
-                            else
-                            {
-                                sb.Append("[ ]\t");
-                                // Console.Write("[ ]\t");
-                            }
-                        // }
-                        // else
-                        // {
-                        //     Console.Write(" " + listNoLed[j] + "\t");
-                        // }
+                        if (listStatusLed[j] == false)
+                        {
+                            listStatusLed[j] = true;
+                        }
+                        else
+                        {
+                            listStatusLed[j] = false;
+                        }
                     }
-                    // Console.WriteLine();
-                // }
-                // Console.Write("Please choose LED to turn On/Off: ");
-                // noLed = Console.ReadLine().ToUpper();
+
+                    if (listStatusLed[j])
+                    {
+                        sb.Append("[!]\t");
+
+                    }
+                    else
+                    {
+                        sb.Append("[ ]\t");
+                    }
+                }
+
             }
             return sb.ToString();
         }
