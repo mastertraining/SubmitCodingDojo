640feb2c83128bf63cfebd359de4a28ca0264e2d
guntza22 <guntza22@hotmail.com>
2018-11-19 09:58:16 +0700

    Done HomeWork5

--- Homework 05/HomeWork5.classlib/HomeWork5ClassLib.cs
+++ Homework 05/HomeWork5.classlib/HomeWork5ClassLib.cs
@@ -7,45 +7,27 @@ namespace HomeWork5.classlib
 {
     public class HomeWork5ClassLib : IHomework05
     {
+        public Dictionary<string,string> listLed { get; set; }
         public string DisplayLEDOnScreen(string ledNo)
-        {
-            var listNoLed = new List<string>(new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "A" });
-            var listStatusLed = new List<bool>(new bool[] { false, false, false, false, false, false, false, false, false, false });
-            var noLed = ledNo.ToUpper();
-            var sb = new StringBuilder();
-            while (true)
-            {
-                // for (int i = 0; i < 2; i++)
-                // {
-                for (int j = 0; j < listNoLed.Count; j++)
-                {
-                    // if (i == 0)
-                    // {
-                    if (noLed == listNoLed[j])
-                    {
-                        if (listStatusLed[j] == false)
-                        {
-                            listStatusLed[j] = true;
-                        }
-                        else
-                        {
-                            listStatusLed[j] = false;
-                        }
-                    }
-
-                    if (listStatusLed[j])
-                    {
-                        sb.Append("[!]\t");
-
-                    }
-                    else
-                    {
-                        sb.Append("[ ]\t");
-                    }
+        {   
+            var ledNoUpper = ledNo.ToUpper();
+            if (listLed.ContainsKey(ledNoUpper)) {
+                if(listLed[ledNoUpper] == "[ ]") {
+                    listLed[ledNoUpper] = "[!]";
                 }
-
+                else {
+                    listLed[ledNoUpper] = "[ ]";
+                }
             }
-            return sb.ToString();
+            var keyLed = listLed.Keys;
+            var valuesLed = listLed.Values;
+            var listKeyLed = String.Join("   ",keyLed);
+            var listValueLed = String.Join(" ",valuesLed);
+
+            var sb = new StringBuilder();
+            var resultDisplayLed = sb.AppendLine(listValueLed).Append(" ").Append(listKeyLed).ToString();
+
+            return resultDisplayLed;
         }
     }
 }
