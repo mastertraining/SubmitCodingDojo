f2fa81859f78d0fdeb1925f7c3d7570944b82866
tatae <tachapong999.gmail.com>
2018-11-19 14:34:15 +0700

    while Loop not complete

--- homework5/homework05/Program.cs
+++ homework5/homework05/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
 
 namespace homework05
 {
@@ -6,59 +8,98 @@ namespace homework05
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("[ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ]");
-            Console.WriteLine(" 1   2   3   4   5   6   7   8   9   A");
-            string input = Console.ReadLine();
-           if (input == "1") 
-           {
-               System.Console.WriteLine("[!] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ]");
-               Console.WriteLine(" 1   2   3   4   5   6   7   8   9   A");
-           }
-            if (input == "2") 
-           {
-               System.Console.WriteLine("[ ] [!] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ]");
-               Console.WriteLine(" 1   2   3   4   5   6   7   8   9   A");
-           }
-             if (input == "3") 
-           {
-               System.Console.WriteLine("[ ] [ ] [!] [ ] [ ] [ ] [ ] [ ] [ ] [ ]");
-               Console.WriteLine(" 1   2   3   4   5   6   7   8   9   A");
-           }
-             if (input == "4") 
-           {
-               System.Console.WriteLine("[ ] [ ] [ ] [!] [ ] [ ] [ ] [ ] [ ] [ ]");
-               Console.WriteLine(" 1   2   3   4   5   6   7   8   9   A");
-           }
-             if (input == "5") 
-           {
-               System.Console.WriteLine("[ ] [ ] [ ] [ ] [!] [ ] [ ] [ ] [ ] [ ]");
-               Console.WriteLine(" 1   2   3   4   5   6   7   8   9   A");
-           }
-             if (input == "6") 
-           {
-               System.Console.WriteLine("[ ] [ ] [ ] [ ] [ ] [!] [ ] [ ] [ ] [ ]");
-               Console.WriteLine(" 1   2   3   4   5   6   7   8   9   A");
-           }
-             if (input == "7") 
-           {
-               System.Console.WriteLine("[ ] [ ] [ ] [ ] [ ] [ ] [!] [ ] [ ] [ ]");
-               Console.WriteLine(" 1   2   3   4   5   6   7   8   9   A");
-           }
-             if (input == "8") 
-           {
-               System.Console.WriteLine("[ ] [ ] [ ] [ ] [ ] [ ] [ ] [!] [ ] [ ]");
-               Console.WriteLine(" 1   2   3   4   5   6   7   8   9   A");
-           }
-             if (input == "9") 
-           {
-               System.Console.WriteLine("[ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [!] [ ]");
-               Console.WriteLine(" 1   2   3   4   5   6   7   8   9   A");
-           }
-             if (input == "a" || input == "A") 
-           {
-               System.Console.WriteLine("[ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [!]");
-               Console.WriteLine(" 1   2   3   4   5   6   7   8   9   A");
-           }
+
+            System.Console.WriteLine("[ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ]");
+            System.Console.WriteLine(" 1   2   3   4   5   6   7   8   9   A");
+
+            // string a = "[ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ]";
+            // string b = "[!] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ]";
+            string number = " 1   2   3   4   5   6   7   8   9   A";
+            // var add = new StringBuilder();
+            // var aadd = add.Append($"{a}\n{number}");
+
+            // string input = Console.ReadLine();
+
+            string c = "[!] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ]";
+            var cadd = new StringBuilder();
+            var caddd = cadd.Append($"{c}\n{number}");
+
+            var input = Console.ReadLine();
+            bool isturnoff = true;
+
+            while (true)
+            {
+
+                {
+                    // if (isturnoff)
+                    {
+                        // System.Console.WriteLine(aadd);
+
+                        if (input == "1")
+                        {
+                            System.Console.WriteLine("[!] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ]");
+                            Console.WriteLine(" 1   2   3   4   5   6   7   8   9   A");
+                        }
+                        if (input == "2")
+                        {
+                            System.Console.WriteLine("[ ] [!] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ]");
+                            Console.WriteLine(" 1   2   3   4   5   6   7   8   9   A");
+                        }
+                        if (input == "3")
+                        {
+                            System.Console.WriteLine("[ ] [ ] [!] [ ] [ ] [ ] [ ] [ ] [ ] [ ]");
+                            Console.WriteLine(" 1   2   3   4   5   6   7   8   9   A");
+                        }
+                        if (input == "4")
+                        {
+                            System.Console.WriteLine("[ ] [ ] [ ] [!] [ ] [ ] [ ] [ ] [ ] [ ]");
+                            Console.WriteLine(" 1   2   3   4   5   6   7   8   9   A");
+                        }
+                        if (input == "5")
+                        {
+                            System.Console.WriteLine("[ ] [ ] [ ] [ ] [!] [ ] [ ] [ ] [ ] [ ]");
+                            Console.WriteLine(" 1   2   3   4   5   6   7   8   9   A");
+                        }
+                        if (input == "6")
+                        {
+                            System.Console.WriteLine("[ ] [ ] [ ] [ ] [ ] [!] [ ] [ ] [ ] [ ]");
+                            Console.WriteLine(" 1   2   3   4   5   6   7   8   9   A");
+                        }
+                        if (input == "7")
+                        {
+                            System.Console.WriteLine("[ ] [ ] [ ] [ ] [ ] [ ] [!] [ ] [ ] [ ]");
+                            Console.WriteLine(" 1   2   3   4   5   6   7   8   9   A");
+                        }
+                        if (input == "8")
+                        {
+                            System.Console.WriteLine("[ ] [ ] [ ] [ ] [ ] [ ] [ ] [!] [ ] [ ]");
+                            Console.WriteLine(" 1   2   3   4   5   6   7   8   9   A");
+                        }
+                        if (input == "9")
+                        {
+                            System.Console.WriteLine("[ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [!] [ ]");
+                            Console.WriteLine(" 1   2   3   4   5   6   7   8   9   A");
+                        }
+                        if (input == "a" || input == "A")
+                        {
+                            System.Console.WriteLine("[ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [!]");
+                            Console.WriteLine(" 1   2   3   4   5   6   7   8   9   A");
+                        }
+
+                        else
+                        {
+                            // System.Console.WriteLine(caddd);
+                            System.Console.WriteLine("Please choose LED to turn On/Off: ");
+                        }
+                        isturnoff = !isturnoff;
+                        input = System.Console.ReadLine();
+                    }
+                }
+                // Console.WriteLine("Exit program");
+
+            }
         }
     }
 }
+
+
