9d484b66dfffcb513eae4b14d362553334af7407
Anawat <anawat@perfenterprise.com>
2018-11-19 17:34:56 +0700

    HW07: Test interface

--- CodingDojo/HomeworkXUnit/Homework07UnitTest.cs
+++ CodingDojo/HomeworkXUnit/Homework07UnitTest.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FluentAssertions;
+using Homework_07;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
@@ -7,16 +9,19 @@ namespace HomeworkXUnit
 {
     public class Homework07UnitTest
     {
+        private IHomework07 IHW;
+
         public Homework07UnitTest()
         {
-
+            IHW = new Homework07();
         }
 
         [Theory]
         [InlineData("hello world and practice makes perfect and hello world again", "again and hello makes perfect practice world")]
         public void RemoveAndSortTextByAlphabeticalShouldWork(string text, string expected)
         {
-
+            var result = IHW.RemoveAndSortTextByAlphabetical(text);
+            result.Should().Be(expected);
         }
     }
 }
