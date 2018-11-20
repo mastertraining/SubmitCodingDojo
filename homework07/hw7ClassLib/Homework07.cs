90f6abfee71bc7b65e6e1cf0ae3522040d1870c0
woraput <woraput@kkumail.com>
2018-11-20 18:53:53 +0700

    setup HW7

---
+++ homework07/hw7ClassLib/Homework07.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hw7ClassLib
+{
+    public class Homework07 : IHomework07
+    {
+        public string RemoveAndSortTextByAlphabetical(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ homework07/hw7Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hw7Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
