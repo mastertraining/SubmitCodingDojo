16e31071407c5f2b5faff1f65b718788b0272ef1
powerofwind <powerofwind10@gmail.com>
2018-11-20 23:57:38 +0700

    rewrite

--- Homework05/Logic.cs
+++ Homework05/Logic.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Text;
 
 namespace Homework05
@@ -7,31 +8,45 @@ namespace Homework05
     {
         public string DisplayLEDOnScreen(string ledNo)
         {
-            var switchh = new string[] { "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]" };
-            var switchhledNo = new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "A" };
-
-            for (int j = 0; j < switchhledNo.Length; j++)
+            var switchh = new bool[] { false, false, false, false, false, false, false, false, false, false };
+            var switchNo = new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "A" };
+            ledNo = ledNo.ToUpper();
+            for (int i = 0; i < switchNo.Length; i++)
             {
-                if (ledNo == switchhledNo[j])
+                if (ledNo == switchNo [i])
                 {
-                    if (switchh[j] == "[ ]")
-                    {
-                        switchh[j] = "[!]";
-                    }
-                    else
-                    {
-                        switchh[j] = "[ ]";
-                    }
+                    switchh.Select(it => it ? "[!]" : "[ ]");
+                //    switchh[i] = !switchh[i];
                 }
             }
 
-            var resultswitchh = string.Join(" ", switchh);
-            var resultswitchhledNo = string.Join("   ", switchhledNo);
-            var result = new StringBuilder();
-            result.Append(resultswitchh).Append(resultswitchhledNo);
 
-            return result.ToString();
-            
+            var collected = new bool[] { false, false, false, false, false, false, false, false, false, false };
+            var selected = collected.Select(it => it ? "[!]" : "[ ]");
+            var b = string.Join("  ", selected);
+            var result = b.ToString();
+            return "result";
+
+            // ledNo = "false";
+            //  bool b = bool.Parse(ledNo);
+            // return logic();
+
+            // var input = new bool[10];
+            // var test = logic.
+
+
+
+
+            // string value2 = "false";
+            // if (value2 == "true")
+            // {
+            //     Console.WriteLine(value2);
+            // }
+
+            // int.TryParse(ledNo, out int ledNumber);
+            // var isSpecialAlphabet = ledNo.Equals("a", StringComparison.CurrentCultureIgnoreCase);
+            // var isNumberValid = ledNumber > 0 && ledNumber < 10;
+            // if (!isNumberValid && !isSpecialAlphabet) return RenderLeds();
         }
     }
 }
--- Homework05/Program.cs
+++ Homework05/Program.cs
@@ -9,36 +9,70 @@ namespace Homework05
     {
         static void Main(string[] args)
         {
-            bool isturnoff = true;
-            string input = "1";
+            // bool isturnoff = true;
+            // string input = "1";
 
-            string a = "[ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ]";
-            string b = "[!] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ]";
-            string number = " 1   2   3   4   5   6   7   8   9   A";
-            var add = new StringBuilder();
-            var aadd = add.Append($"{a}\n{number}");
+            // string a = "[ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ]";
+            // string number = " 1   2   3   4   5   6   7   8   9   A";
+            // var add = new StringBuilder();
+            // var aadd = add.Append($"{a}\n{number}");
 
-            string c = "[!] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ]";
-            var cadd = new StringBuilder();
-            var caddd = cadd.Append($"{c}\n{number}");
+            // string c = "[!] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ]";
+            // var cadd = new StringBuilder();
+            // var caddd = cadd.Append($"{c}\n{number}");
 
-            while (input == "1")
+            // while (input == "1")
+            // {
+            //     if (isturnoff)
+            //     {
+            //         System.Console.WriteLine(aadd);
+            //         System.Console.WriteLine("Please choose LED to turn On: ");
+            //     }
+            //     else
+            //     {
+            //         System.Console.WriteLine(caddd);
+            //         System.Console.WriteLine("Please choose LED to turn On/Off: ");
+            //     }
+            //     isturnoff = !isturnoff;
+            //     input = System.Console.ReadLine();
+
+            // }
+            // Console.WriteLine("Exit program");
+
+
+            var input = System.Console.ReadLine();
+
+            var test = new Logic();
+            var result = test.DisplayLEDOnScreen(input);
+
+            System.Console.Write(result);
+
+
+
+            var ledNo = System.Console.ReadLine();
+            //var switchh = new bool[] { false, false, false, false, false, false, false, false, false, false };
+            string[] switchhB = new string[] { "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]" };
+            var switchNo = new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "A" };
+            ledNo = ledNo.ToUpper();
+            for (int i = 0; i < switchNo.Length; i++)
             {
-                if (isturnoff)
+                if (ledNo == switchNo[i])
                 {
-                System.Console.WriteLine(aadd);
-               System.Console.WriteLine("Please choose LED to turn On: ");
-                }
-                else
-                {
-                    System.Console.WriteLine(caddd);
-                    System.Console.WriteLine("Please choose LED to turn On/Off: ");
-                }
-                isturnoff = !isturnoff;
-              input = System.Console.ReadLine();
+                    switchhB[i] = !switchhB[i];
 
+                    Console.WriteLine(a);
+                }
             }
-            Console.WriteLine("Exit program");
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
