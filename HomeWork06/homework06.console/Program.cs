79c8eac3a7dcd6c9c0637fd62f59a1c8470360d6
Earn <earndarkness@perfenterprise.com>
2018-11-20 17:51:07 +0700

    Implement Homework06 follow list: 1. DisplayLEDOnScreen 2. LoadState 3. CreateDefaultStateFile 4. Using Data from file 'config.yaml'
    Signed-off-by: Earn <earndarkness@perfenterprise.com>

---
+++ HomeWork06/homework06.console/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using homework06.lib;
+
+namespace homework06.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var displayMessage = new List<string>();
+            var announceText = $"Hello Welcome to DisplayLEDOnScreen analysis (HomeWork 05)";
+            var rulesText = $"Rules:{Environment.NewLine}\tInput: 1 - 9 and 'A' or 'a' only{Environment.NewLine}\tInput: 0 for exit program";
+            var ledScreen = $"Led Screen:{Environment.NewLine}\t[ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ]{Environment.NewLine}\t 1   2   3   4   5   6   7   8   9   A";
+
+            displayMessage.Add(announceText);
+            displayMessage.Add(rulesText);
+            displayMessage.Add(ledScreen);
+            var message = string.Join(Environment.NewLine, displayMessage);
+            System.Console.Write(message);
+
+            var ledOnScreen = new Homework06();
+            var isContinueInputLedOnScreen = true;
+            while (isContinueInputLedOnScreen)
+            {
+                System.Console.Write($"{Environment.NewLine}Please choose LED to turn On/Off: ");
+                var input = Console.ReadLine();
+                if (input.Equals("0")) isContinueInputLedOnScreen = false;
+                else
+                {
+                    var displayLed = ledOnScreen.DisplayLEDOnScreen(input);
+                    System.Console.WriteLine(displayLed);
+                }
+            }
+            System.Console.WriteLine("Exit program!");
+        }
+    }
+}
---
+++ HomeWork06/homework06.lib/Homework06.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace homework06.lib
+{
+    public class Homework06 : IHomework06
+    {
+        private const string FileName = @"config.yaml";
+        private string _symbolOn;
+        private string _symbolOff;
+        private int _spaceDistanceAmount;
+        private const int DefaultspaceDistanceAmount = 3;
+
+        private bool[] _leds;
+
+        public Homework06()
+        {
+            LoadState();
+            _leds = new bool[10];
+        }
+
+        public string DisplayLEDOnScreen(string ledNo)
+        {
+            var isLedNoValid = !string.IsNullOrWhiteSpace(ledNo);
+            if (!isLedNoValid) return RenderLeds();
+
+            int.TryParse(ledNo, out int ledNumber);
+            var isSpecialAlphabet = ledNo.Equals("a", StringComparison.CurrentCultureIgnoreCase);
+            var isNumberValid = ledNumber > 0 && ledNumber < 10;
+            if (!isNumberValid && !isSpecialAlphabet) return RenderLeds();
+
+            if (isSpecialAlphabet) ledNumber = 10;
+            var toggleLedNumber = ledNumber - 1;
+            _leds[toggleLedNumber] = !_leds[toggleLedNumber];
+            return RenderLeds();
+        }
+
+        public string LoadState()
+        {
+            var result = string.Empty;
+            if (!File.Exists(FileName)) CreateDefaultStateFile();
+            using (var file = File.OpenText(FileName))
+            {
+                result = file.ReadToEnd();
+                var textEachLine = result.Split('\n');
+                for (int i = 0; i < textEachLine.Count(); i++)
+                {
+                    var value = textEachLine[i].Split(':').LastOrDefault().Trim();
+                    switch (i)
+                    {
+                        case 0: _symbolOn = value; break;
+                        case 1: _symbolOff = value; break;
+                        case 2: _spaceDistanceAmount = int.Parse(value); break;
+                        default: break;
+                    }
+                }
+            }
+            return result;
+        }
+
+        public void SaveCurrentState()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetAppConfigurations(string onSymbol, string offSymbol, int spacing)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string RenderLeds()
+        {
+            Func<bool, string> ConvertToLedFormat = (it) =>
+            {
+                var symbolOnDisplay = string.IsNullOrWhiteSpace(_symbolOn) ? " " : _symbolOn;
+                var symbolOffDisplay = string.IsNullOrWhiteSpace(_symbolOff) ? " " : _symbolOff;
+                return it ? $"[{symbolOnDisplay}]" : $"[{symbolOffDisplay}]";
+            };
+
+            var spaceLedAmount = new string(' ', _spaceDistanceAmount);
+            var speaceLedNumberAmount = new string(' ', _spaceDistanceAmount - 1 > 0 ? DefaultspaceDistanceAmount + (_spaceDistanceAmount - 1) : DefaultspaceDistanceAmount);
+
+            var ledDisplay = _leds.Select(ConvertToLedFormat);
+            var ledRow = string.Join(spaceLedAmount, ledDisplay);
+            var numberRow = $"{Environment.NewLine} 1{speaceLedNumberAmount}2{speaceLedNumberAmount}3{speaceLedNumberAmount}4{speaceLedNumberAmount}5{speaceLedNumberAmount}6{speaceLedNumberAmount}7{speaceLedNumberAmount}8{speaceLedNumberAmount}9{speaceLedNumberAmount}A";
+
+            var render = new StringBuilder();
+            render.Append(ledRow);
+            render.Append(numberRow);
+            return render.ToString();
+        }
+
+        private void CreateDefaultStateFile()
+        {
+            using (var file = File.Create(FileName))
+            {
+                var defaultState = new StringBuilder();
+                defaultState.AppendLine("on-symbol : !")
+                            .AppendLine("off-symbol :  ")
+                            .AppendLine("spaces : 3");
+                var info = new UTF8Encoding(true).GetBytes(defaultState.ToString());
+                file.Write(info, 0, info.Length);
+            }
+        }
+    }
+}
