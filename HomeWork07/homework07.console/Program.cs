b33a99b9fd960902cf19a6632af5e45deebfafad
Earn <earndarkness@perfenterprise.com>
2018-11-20 16:32:05 +0700

    Implement Homework07
    Signed-off-by: Earn <earndarkness@perfenterprise.com>

---
+++ HomeWork07/homework07.console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using homework07.lib;
+
+namespace homework07.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello Welcome to RemoveAndSortTextByAlphabetical analysis (HomeWork 07)\nWhich text would you want to rmove and sort text by alphabetical?: ");
+            var text = Console.ReadLine();
+            var control = new Homework07();
+            var result = control.RemoveAndSortTextByAlphabetical(text);
+            System.Console.WriteLine($"Result: {result}");
+        }
+    }
+}
---
+++ HomeWork07/homework07.lib/Homework07.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace homework07.lib
+{
+    public class Homework07 : IHomework07
+    {
+        public string RemoveAndSortTextByAlphabetical(string text)
+        {
+            var isTextValid = !string.IsNullOrWhiteSpace(text);
+            if (!isTextValid) return string.Empty;
+
+            var textSplited = text.Trim().Split().Distinct().OrderBy(it => it);
+            return string.Join(" ", textSplited);
+        }
+    }
+}
