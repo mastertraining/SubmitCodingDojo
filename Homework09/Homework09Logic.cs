49e151983d5932415916b105a50cf234c483de98
powerofwind <powerofwind10@gmail.com>
2018-11-22 01:16:26 +0700

    build constractor and show .CSV data

--- Homework09/Homework09Logic.cs
+++ Homework09/Homework09Logic.cs
@@ -1,12 +1,41 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Homework09
 {
     class Homework09Logic : IHomework09
     {
         private const string FileName = @"product.csv";
-        
+        private List<Product> _products;
+
+        public Homework09Logic()
+        {
+            _products = new List<Product>();
+            LoadCSVFile();
+        }
+
+        private void LoadCSVFile()
+        {
+            using (var file = File.OpenText(FileName))
+            {
+                var allText = file.ReadToEnd().Split('\n').Skip(1);
+                foreach (var item in allText)
+                {
+                    var data = item.Split(',');
+                    var product = new Product
+                    {
+                        SKU = data[0],
+                        Name = data[1],
+                        Price = double.Parse(data[2])
+                    };
+                    _products.Add(product);
+                    System.Console.WriteLine(item);
+                }
+            }
+        }
+
         public void AddProductToCart(IProduct product)
         {
             throw new NotImplementedException();
@@ -23,7 +52,7 @@ namespace Homework09
         }
     }
 
-    class Homework09LogicIProduct : IProduct
+    public class Product : IProduct
     {
         public string SKU { get; set; }
         public string Name { get; set; }
--- Homework09/ProgramHomework09.cs
+++ Homework09/ProgramHomework09.cs
@@ -5,11 +5,12 @@ namespace Homework09
     class ProgramHomework09
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Products in your cart are");
-            Console.ReadLine();
-            Console.WriteLine("Total amount:");
-            Console.WriteLine("Please input a product key:");
+        { 
+            var a = new Homework09Logic();
+            // Console.WriteLine("Products in your cart are");
+            // Console.ReadLine();
+            // Console.WriteLine("Total amount:");
+            // Console.WriteLine("Please input a product key:");
 
         }
     }
