b27187e84d5389ea03114c39607d358757f3f209
woraput <woraput@kkumail.com>
2018-11-21 21:58:23 +0700

    setup hw8 hw9

---
+++ homework08/hw8ClassLib/Homework08.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hw8ClassLib
+{
+    public class Homework08 : IHomework08
+    {
+        public string GetTriangleShapeAsText(int input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
