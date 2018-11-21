9159fa76162757998512bcbd8c857a937f21ad58
thaninpong <thaninpong@perfenterprise.com>
2018-11-21 16:10:23 +0700

    Implement HomeWork07

---
+++ HomeWork07/HW07.lib/HW07.lib/Homework.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace HW07.lib
+{
+    public class Homework : IHomework07
+    {
+        public string RemoveAndSortTextByAlphabetical(string text)
+        {
+            var data = text.Split(' ').ToList();
+            data = data.Distinct().OrderBy(it => it).ToList();
+            return string.Join(" ", data);
+        }
+    }
+}
