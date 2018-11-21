41f3fc63e693b5ef48dfaa321e19146e8c06d870
Earn <earndarkness@perfenterprise.com>
2018-11-21 19:09:22 +0700

    Implement Homework09
    Signed-off-by: Earn <earndarkness@perfenterprise.com>

---
+++ HomeWork09/homework09.lib/Homework09.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace homework09.lib
+{
+    public class Homework09 : IHomework09
+    {
+        private const string FileName = @"product.csv";
+        private List<Product> _products;
+        private List<Product> _orderProducts;
+
+        public Homework09()
+        {
+            _products = new List<Product>();
+            _orderProducts = new List<Product>();
+            LoadProducts();
+        }
+
+        public void AddProductToCart(IProduct product)
+        {
+            var isDataValid = product != null;
+            if (!isDataValid) return;
+
+            _orderProducts.Add(new Product(product));
+        }
+
+        public IEnumerable<IProduct> GetAllProducts()
+        {
+            return _products;
+        }
+
+        public IEnumerable<IProduct> GetProductsInCart()
+        {
+            return _orderProducts;
+        }
+
+        private void LoadProducts()
+        {
+            using (var file = File.OpenText(FileName))
+            {
+                var textEachLine = file.ReadToEnd().Split('\n');
+                foreach (var item in textEachLine.Skip(1))
+                {
+                    var data = item.Split(',');
+                    var sku = data[0];
+                    var name = data[1];
+                    var price = int.Parse(data[2]);
+                    _products.Add(new Product(sku, name, price));
+                }
+            }
+        }
+    }
+}
---
+++ HomeWork09/homework09.test/LoadProductTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace homework09.test
+{
+    public class LoadProductTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
