eba206c411082360a660020e09e1bfb4ffe504ae
rn0z <manutsawee113@gmail.com>
2018-11-21 10:12:58 +0700

    Preparing homeworks.

--- Homework6/Program.cs
+++ Homework6/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
 
 namespace Homework6
 {
-    class Program
+    class Program: IHomework06
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
+
+        public void SetAppConfigurations(string onSymbol, string offSymbol, int spacing)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string DisplayLEDOnScreen(string ledNo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string LoadState()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveCurrentState()
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
