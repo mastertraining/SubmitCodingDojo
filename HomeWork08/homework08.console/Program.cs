73dceb2050d6bc7911801357d436cef48b49facd
Earn <earndarkness@perfenterprise.com>
2018-11-21 18:11:40 +0700

    Implement Homework08
    Signed-off-by: Earn <earndarkness@perfenterprise.com>

---
+++ HomeWork08/homework08.console/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using homework08.lib;
+
+namespace homework08.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write($"Hello Welcome to GetTriangleShapeAsText analysis (HomeWork 08){Environment.NewLine}How many do you want to build TriangleShape height?: ");
+            int.TryParse(Console.ReadLine(), out int input);
+
+            var isValidInput = input > 0;
+            if (!isValidInput)
+            {
+                System.Console.WriteLine("Your input must more than or equal 1.");
+                return;
+            }
+
+            var control = new Homework08();
+            var result = control.GetTriangleShapeAsText(input);
+            System.Console.WriteLine($"Result{Environment.NewLine}{result}");
+
+        }
+    }
+}
---
+++ HomeWork08/homework08.lib/Homework08.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace homework08.lib
+{
+    public class Homework08 : IHomework08
+    {
+        public string GetTriangleShapeAsText(int input)
+        {
+            const int MinimumInput = 1;
+            var isInputValid = input >= MinimumInput;
+            if (!isInputValid) return string.Empty;
+
+            var result = new StringBuilder();
+            for (int i = 1; i <= input; i++)
+            {
+                var diagonalShape = "/".PadLeft((input - i) + 1);
+                var isBaseTriangle = i == input;
+                var baseLineTriangle = $"{new string('_', input - 1)}|";
+                var perpendicularShape = isBaseTriangle ? baseLineTriangle : "|".PadLeft(i);
+                var shape = $"{diagonalShape}{perpendicularShape}";
+                result.AppendLine(shape);
+            }
+            return result.ToString();
+        }
+    }
+}
