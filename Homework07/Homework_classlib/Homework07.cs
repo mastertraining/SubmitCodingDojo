a009168bb635ef7dcd22c69c05f32498d635e54b
sirisak <sirisak022602020sunon@gmail.com>
2018-11-21 13:53:58 +0700

    Homework07

---
+++ Homework07/Homework_classlib/Homework07.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace Homework_classlib 
+{
+    public class Homework07 : IHomework07
+    {
+        public string RemoveAndSortTextByAlphabetical(string text)
+        {
+            var newTextSplit = text.Split(' ');
+            Array.Sort(newTextSplit);
+            var distinct = newTextSplit.Distinct();
+            return  string.Join(" ", distinct);  
+        }
+    }
+}
