2efbc7634c26fcd7d346f2365f889e31ad1ce22c
Kritsana <tragoolphrom@gmail.com>
2018-11-21 16:35:55 +0700

    HomeWork06 Finish

---
+++ HomeWork06/Homework.ConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Homework.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var LED = new Homework06();
+            Console.WriteLine(LED.LoadState());
+            Console.Write("Please choose LED to turn On/Off: ");
+            string ledNo;
+            do
+            {
+                ledNo = Console.ReadLine();
+                if (ledNo == "save")
+                {
+                    LED.SaveCurrentState();
+                    break;
+                }
+                var displayLED = LED.DisplayLEDOnScreen(ledNo);
+                Console.WriteLine(displayLED);
+                Console.Write("Please choose LED to turn On/Off: ");
+            } while (!(string.IsNullOrWhiteSpace(ledNo)));
+        }
+    }
+}
