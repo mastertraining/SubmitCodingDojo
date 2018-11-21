b3aa1515555f94ab526fce5e6b0e5b0e12d69ad6
guntza22 <guntza22@hotmail.com>
2018-11-22 00:08:52 +0700

    Done HomeWork8

---
+++ Homework 08/HomeWork8.console/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using HomeWork8.classlib;
+
+namespace HomeWork8.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hw8 = new HomeWork8ClassLib();
+            while (true)
+            {
+                Console.Write("Enter number: ");
+                var number = int.Parse(Console.ReadLine());
+                var result = hw8.GetTriangleShapeAsText(number);
+                Console.Write(result);
+            }
+        }
+    }
+}
