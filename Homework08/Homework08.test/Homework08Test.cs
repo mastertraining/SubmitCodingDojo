aadfe0b1b6b40059181b46fe1345222f85c96c1c
Perawatt <pekung4433@perfenterprise.com>
2018-11-22 12:23:59 +0700

    completed Homework08

---
+++ Homework08/Homework08.test/Homework08Test.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+
+namespace Homework08.test
+{
+    public class Homework08Test
+    {
+        [Theory]
+        [InlineData(5, "    /|\r\n   / |\r\n  /  |\r\n /   |\r\n/____|")]
+        [InlineData(3, "  /|\r\n / |\r\n/__|")]
+        [InlineData(1, "Please enter a number that is greater than 1.")]
+        public void CreateTriangleTest(int input,string expected)
+        {
+            var sut = new Homework08();
+            var result = sut.GetTriangleShapeAsText(input);
+            Assert.Equal(expected, result);
+        }
+    }
+}
