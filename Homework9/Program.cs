3bb08e531f7845fe40c14db820312b3459376465
rn0z <manutsawee113@gmail.com>
2018-11-23 12:16:49 +0700

    running Homework9, Now 5%

--- Homework9/Program.cs
+++ Homework9/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Homework9
 {
-    class Program
+    class Program: IHomework09
     {
+        List<IProduct> products { get; set; }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        public void AddProductToCart(IProduct product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<IProduct> GetAllProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<IProduct> GetProductsInCart()
+        {
+            throw new NotImplementedException();
         }
     }
 }
