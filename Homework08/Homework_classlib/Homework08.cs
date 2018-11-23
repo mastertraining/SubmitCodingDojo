f24bd6d8e36837c552dd9fa13d5074d6a155f20b
sirisak <sirisak022602020sunon@gmail.com>
2018-11-23 16:30:33 +0700

    Homework08

---
+++ Homework08/Homework_classlib/Homework08.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Homework_classlib
+{
+    public class Homework08 : IHomework08
+    {
+        public string GetTriangleShapeAsText(int input)
+        {
+            var build = new StringBuilder();
+
+            for (int i = 1; i <= input; i++)
+            {
+                var space = input - i;
+                var underLine = i - 1;
+                var symbol = ' ';
+                for (int j = 1; j <= space; j++)
+                {
+                    build.Append(' ');
+                }
+                build.Append('/');
+                for (int k = 1; k <= underLine; k++)
+                {
+                    if (i == input)
+                    {
+                        symbol = '_';
+                    }
+                    else
+                    {
+                        symbol = ' ';
+                    }
+                    build.Append(symbol);
+                }
+                build.AppendLine("|");
+            }
+
+            return build.ToString();
+        }
+    }
+}
---
+++ Homework08/Homework_console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Homework_classlib;
+
+namespace Homework_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input number : ");
+            var input = int.Parse(Console.ReadLine());
+            var call = new Homework08();
+            var newText = call.GetTriangleShapeAsText(input);
+            Console.WriteLine(newText);
+        }
+    }
+}
