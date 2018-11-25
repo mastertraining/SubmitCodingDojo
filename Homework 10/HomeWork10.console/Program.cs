96bc1a9733434024b00071e32a73d1ac5111c625
guntza22 <guntza22@hotmail.com>
2018-11-25 21:28:24 +0700

    not done

--- Homework 10/HomeWork10.console/Program.cs
+++ Homework 10/HomeWork10.console/Program.cs
@@ -14,6 +14,8 @@ namespace HomeWork10.console
             var skuNo = "none";
             var amount = 0;
             var balance = 0.00;
+            var purchase = new List<string>();
+            var checkStringToInt = true;
             foreach (var products in getAllProduct)
             {
                 Console.WriteLine($"{products.SKU} , {products.Name} , {products.Price,2:N}");
@@ -22,7 +24,7 @@ namespace HomeWork10.console
 
             while (true)
             {
-                var getProductInCart = hw10.GetProductsInCart();
+                var getProductInCart = hw10.GetProductsInCart().ToList();
                 Console.WriteLine("Products in your cart are");
                 if (!getProductInCart.Any())
                 {
@@ -33,7 +35,7 @@ namespace HomeWork10.console
                     var count = 1;
                     foreach (var productCart in getProductInCart)
                     {
-                        Console.WriteLine($"{count}.{productCart.Name}\t({productCart.amount}) {productCart.Price,2:N}");
+                        Console.WriteLine($"{count}.{productCart.Name}\t({hw10.countProduct[productCart.SKU]}) {productCart.Price,2:N}");
                         count++;
                     }
                 }
@@ -43,18 +45,38 @@ namespace HomeWork10.console
                 var inputPurchase = Console.ReadLine();
                 if (inputPurchase.Contains(','))
                 {
-                    var purchase = inputPurchase.Split(',').ToList();
-                    int.TryParse(purchase[1], out int amountProduct);
+                    purchase = inputPurchase.Split(',').ToList();
+                    checkStringToInt = int.TryParse(purchase[1], out int amountProduct);
                     skuNo = purchase[0];
-                    amount = amountProduct;
+                    amount = checkStringToInt ? amountProduct : amountProduct;
+                    // amount = amountProduct;
+                    hw10.countProduct["skuNo"] += amount;
+                }
+                else
+                {
+                    skuNo = inputPurchase;
+                    hw10.countProduct[skuNo] = amount + 1;
                 }
                 var addToCart = getAllProduct.FirstOrDefault(it => it.SKU == skuNo);
-                hw10.AddProductToCart(addToCart);
-              
-                balance += addToCart.Price * amount;
+                var checkDuplicateProduct = hw10.listCart.Select(it => it.SKU == skuNo).ToList();
+                if (checkDuplicateProduct.Any())
+                {
+                    // foreach (var productCart in getProductInCart) {
+                    //     productCart.SKU = skuNo;
+                    //     productCart.Add(addToCart);
+                    // }
+                    // getProductInCart.Remove(addToCart);
+                    hw10.countProduct[skuNo] += amount;
+                    // hw10.AddProductToCart(addToCart);
+                }
+                else
+                {
+                    hw10.AddProductToCart(addToCart);
+                    hw10.countProduct[skuNo] += amount;
+                }
+                balance += addToCart.Price * hw10.countProduct[skuNo];
                 Console.WriteLine(new string('=', 60));
             }
-
         }
     }
 }
