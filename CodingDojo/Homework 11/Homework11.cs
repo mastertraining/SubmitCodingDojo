d8f7e78f01c2820a3299b2497fcec94067d6c7b1
Anawat <anawat@perfenterprise.com>
2018-11-26 18:05:18 +0700

    setup HW11

---
+++ CodingDojo/Homework 11/Homework11.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework_11
+{
+    public class Homework11 : IHomework11
+    {
+        public string GetReadWordOfNumber(int number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CodingDojo/HomeworkXUnit/Homework11UnitTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using FluentAssertions;
+
+namespace HomeworkXUnit
+{
+    public class Homework11UnitTest
+    {
+    }
+}
