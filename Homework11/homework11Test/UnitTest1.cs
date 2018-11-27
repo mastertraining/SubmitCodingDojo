ffdf622df03f1b96b14ff9575665469f4d0fae93
powerofwind <powerofwind10@gmail.com>
2018-11-27 21:46:17 +0700

    implement code twodigit

--- Homework11/homework11Test/UnitTest1.cs
+++ Homework11/homework11Test/UnitTest1.cs
@@ -8,7 +8,24 @@ namespace homework11Test
     {
         [Theory (DisplayName="เลขหนึ่งหลัก")]
         [InlineData (1,"หนึ่ง")]
+        [InlineData (0,"ศูนย์")]
+        [InlineData (8,"แปด")]
+
+
         public void onedigit(int number, string expected)
+        {
+            var sut = new Logic();
+            var result = sut.GetReadWordOfNumber(number);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory (DisplayName="เลขสองหลักลงท้ายที่สิบ")]
+        [InlineData (10,"สิบ")]
+        [InlineData (20,"ยี่สิบ")]
+        [InlineData (80,"แปดสิบ")]
+
+
+        public void twodigit(int number, string expected)
         {
             var sut = new Logic();
             var result = sut.GetReadWordOfNumber(number);
--- Homework11/homework11class/Logic.cs
+++ Homework11/homework11class/Logic.cs
@@ -5,8 +5,93 @@ namespace homework11class
     public class Logic : IHomework11
     {
         public string GetReadWordOfNumber(int number)
-        {
-            throw new NotImplementedException();
+        {   //12
+            var text = number.ToString();
+            if (text.Length == 2)
+            {
+               if (text == "90")
+                {
+                    return "เก้าสิบ";
+                }
+                if (text == "80")
+                {
+                    return "แปดสิบ";
+                }
+                if (text == "70")
+                {
+                    return "เจ็ดสิบ";
+                }
+                if (text == "60")
+                {
+                    return "หกสิบ";
+                }
+                if (text == "50")
+                {
+                    return "ห้าสิบ";
+                }
+                if (text == "40")
+                {
+                    return "สี่สิบ";
+                }
+                if (text == "30")
+                {
+                    return "สามสิบ";
+                }
+                if (text == "20")
+                {
+                    return "ยี่สิบ";
+                }
+                if (text == "10")
+                {
+                    return "สิบ";
+                } 
+            }
+            else
+            {
+                if (text == "9")
+                {
+                    return "เก้า";
+                }
+                if (text == "8")
+                {
+                    return "แปด";
+                }
+                if (text == "7")
+                {
+                    return "เจ็ด";
+                }
+                if (text == "6")
+                {
+                    return "หก";
+                }
+                if (text == "5")
+                {
+                    return "ห้า";
+                }
+                if (text == "4")
+                {
+                    return "สี่";
+                }
+                if (text == "3")
+                {
+                    return "สาม";
+                }
+                if (text == "2")
+                {
+                    return "สอง";
+                }
+                if (text == "1")
+                {
+                    return "หนึ่ง";
+                }
+                if (text == "0")
+                {
+                    return "ศูนย์";
+                }
+            }
+
+            return "";
+
         }
     }
 }
