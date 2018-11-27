802c77c8723b7315af13e36afff462e0ac871d92
aphaphon <pui.aphaphon@gmail.com>
2018-11-28 02:24:47 +0700

    Implement all test completed

--- HomeWork 11/homework11.Test/GetReadWordOfNumberTest.cs
+++ HomeWork 11/homework11.Test/GetReadWordOfNumberTest.cs
@@ -5,55 +5,100 @@ namespace homework11.Test
 {
     public class GetReadWordOfNumberTest
     {
-        [Theory(DisplayName="ตัวเลขหลักหน่วย")]
-        [InlineData(0,"ศูนย์")]
-        [InlineData(1,"หนึ่ง")]
-        [InlineData(2,"สอง")]
-        [InlineData(3,"สาม")]
-        [InlineData(4,"สี่")]
-        [InlineData(5,"ห้า")]
-        [InlineData(6,"หก")]
-        [InlineData(7,"เจ็ด")]
-        [InlineData(8,"แปด")]
-        [InlineData(9,"เก้า")]
-        public void CheckGetReadWordOfNumber1Digit(int inPutNumber,string expected)
+        [Theory(DisplayName = "ตัวเลขหลักหน่วย0-9")]
+        [InlineData(0, "ศูนย์")]
+        [InlineData(1, "หนึ่ง")]
+        [InlineData(2, "สอง")]
+        [InlineData(3, "สาม")]
+        [InlineData(4, "สี่")]
+        [InlineData(5, "ห้า")]
+        [InlineData(6, "หก")]
+        [InlineData(7, "เจ็ด")]
+        [InlineData(8, "แปด")]
+        [InlineData(9, "เก้า")]
+        public void CheckGetReadWordOfNumber1Digit(int inPutNumber, string expected)
         {
             var sut = new GetReadWordOfNumberLogic();
             var result = sut.GetReadWordOfNumber(inPutNumber);
-            Assert.Equal(expected,result);
-        }
-       
-        [Theory(DisplayName="ตัวเลขหลักสิบ")]
-        [InlineData(10,"สิบ")]
-        [InlineData(20,"ยี่สิบ")]
-        [InlineData(30,"สามสิบ")]
-        [InlineData(40,"สี่สิบ")]
-        [InlineData(50,"ห้าสิบ")]
-        [InlineData(60,"หกสิบ")]
-        [InlineData(70,"เจ็ดสิบ")]
-        [InlineData(80,"แปดสิบ")]
-        [InlineData(90,"เก้าสิบ")]
-        public void CheckGetReadWordOfNumber2Digit(int inPutNumber,string expected)
-        {
-            var sut = new GetReadWordOfNumberLogic();
-            var result = sut.GetReadWordOfNumber(inPutNumber);
-            Assert.Equal(expected,result);
+            Assert.Equal(expected, result);
         }
 
-        [Theory(DisplayName="ตัวเลขหลักสิบ")]
-        [InlineData(12,"สิบสอง")]
-        [InlineData(13,"สิบสาม")]
-        [InlineData(14,"สิบสี่")]
-        [InlineData(15,"สิบห้า")]
-        [InlineData(16,"สิบหก")]
-        [InlineData(17,"สิบเจ็ด")]
-        [InlineData(18,"สิบแปด")]
-        [InlineData(19,"สิบเก้า")]
-        public void CheckGetReadWordOfNumber2Digits(int inPutNumber,string expected)
+        [Theory(DisplayName = "ตัวเลขหลักสิบทั้งหมด10-90")]
+        [InlineData(10, "สิบ")]
+        [InlineData(20, "ยี่สิบ")]
+        [InlineData(30, "สามสิบ")]
+        [InlineData(40, "สี่สิบ")]
+        [InlineData(50, "ห้าสิบ")]
+        [InlineData(60, "หกสิบ")]
+        [InlineData(70, "เจ็ดสิบ")]
+        [InlineData(80, "แปดสิบ")]
+        [InlineData(90, "เก้าสิบ")]
+        public void CheckGetReadWordOfNumber2Digit(int inPutNumber, string expected)
         {
             var sut = new GetReadWordOfNumberLogic();
             var result = sut.GetReadWordOfNumber(inPutNumber);
-            Assert.Equal(expected,result);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory(DisplayName = "ตัวเลขหลักสิบ12-19")]
+        [InlineData(11, "สิบเอ็ด")]
+        [InlineData(12, "สิบสอง")]
+        [InlineData(13, "สิบสาม")]
+        [InlineData(14, "สิบสี่")]
+        [InlineData(15, "สิบห้า")]
+        [InlineData(16, "สิบหก")]
+        [InlineData(17, "สิบเจ็ด")]
+        [InlineData(18, "สิบแปด")]
+        [InlineData(19, "สิบเก้า")]
+        [InlineData(21, "ยี่สิบเอ็ด")]
+        [InlineData(22, "ยี่สิบสอง")]
+        [InlineData(23, "ยี่สิบสาม")]
+        [InlineData(24, "ยี่สิบสี่")]
+        [InlineData(25, "ยี่สิบห้า")]
+        [InlineData(26, "ยี่สิบหก")]
+        [InlineData(27, "ยี่สิบเจ็ด")]
+        [InlineData(28, "ยี่สิบแปด")]
+        [InlineData(29, "ยี่สิบเก้า")]
+        [InlineData(31, "สามสิบเอ็ด")]
+        [InlineData(32, "สามสิบสอง")]
+        [InlineData(33, "สามสิบสาม")]
+        [InlineData(34, "สามสิบสี่")]
+        [InlineData(35, "สามสิบห้า")]
+        [InlineData(36, "สามสิบหก")]
+        [InlineData(37, "สามสิบเจ็ด")]
+        [InlineData(38, "สามสิบแปด")]
+        [InlineData(39, "สามสิบเก้า")]
+        public void CheckGetReadWordOfNumber2Digits(int inPutNumber, string expected)
+        {
+            var sut = new GetReadWordOfNumberLogic();
+            var result = sut.GetReadWordOfNumber(inPutNumber);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory(DisplayName = "ตัวเลขหลักร้อยทั้งหมด100-900")]
+        [InlineData(100, "หนึ่งร้อย")]
+        [InlineData(200, "สองร้อย")]
+        [InlineData(300, "สามร้อย")]
+        [InlineData(400, "สี่ร้อย")]
+        [InlineData(500, "ห้าร้อย")]
+        [InlineData(600, "หกร้อย")]
+        [InlineData(700, "เจ็ดร้อย")]
+        [InlineData(800, "แปดร้อย")]
+        [InlineData(900, "เก้าร้อย")]
+        [InlineData(125, "หนึ่งร้อยยี่สิบห้า")]
+        [InlineData(205, "สองร้อยห้า")]
+        [InlineData(1000, "หนึ่งพัน")]
+        [InlineData(10000, "หนึ่งหมื่น")]
+        [InlineData(19750, "หนึ่งหมื่นเก้าพันเจ็ดร้อยห้าสิบ")]
+        [InlineData(100000, "หนึ่งแสน")]
+        [InlineData(102536, "หนึ่งแสนสองพันห้าร้อยสามสิบหก")]
+        [InlineData(1000000, "หนึ่งล้าน")]
+        [InlineData(1235478, "หนึ่งล้านสองแสนสามหมื่นห้าพันสี่ร้อยเจ็ดสิบแปด")]
+        public void CheckGetReadWordOfNumber3Digit(int inPutNumber, string expected)
+        {
+            var sut = new GetReadWordOfNumberLogic();
+            var result = sut.GetReadWordOfNumber(inPutNumber);
+            Assert.Equal(expected, result);
         }
     }
 }
--- HomeWork 11/homework11/GetReadWordOfNumberLogic.cs
+++ HomeWork 11/homework11/GetReadWordOfNumberLogic.cs
@@ -8,9 +8,73 @@ namespace homework11
         public string GetReadWordOfNumber(int number)
         {
             var strNumber = number.ToString();
-            if (strNumber.Length == 2)
+            var result = "";
+            if (strNumber.Length == 7)
             {
-                var result = "";
+                var million = Get1digit(strNumber[0]);
+                if (number % 1000000 == 0)
+                {
+                    return ($"{million}ล้าน");
+                }
+                else
+                {
+                    return ($"{million}ล้าน{GetReadWordOfNumber(number % 1000000)}");
+                }
+            }
+
+            else if (strNumber.Length == 6)
+            {
+                var hundredThousand = Get1digit(strNumber[0]);
+                if (number % 100000 == 0)
+                {
+                    return ($"{hundredThousand}แสน");
+                }
+                else
+                {
+                    return ($"{hundredThousand}แสน{GetReadWordOfNumber(number % 100000)}");
+                }
+            }
+
+            else if (strNumber.Length == 5)
+            {
+                var tenThousand = Get1digit(strNumber[0]);
+                if (number % 10000 == 0)
+                {
+                    return ($"{tenThousand}หมื่น");
+                }
+                else
+                {
+                    return ($"{tenThousand}หมื่น{GetReadWordOfNumber(number % 10000)}");
+                }
+            }
+
+            else if (strNumber.Length == 4)
+            {
+                var thousand = Get1digit(strNumber[0]);
+                if (number % 1000 == 0)
+                {
+                    return ($"{thousand}พัน");
+                }
+                else
+                {
+                    return ($"{thousand}พัน{GetReadWordOfNumber(number % 1000)}");
+                }
+            }
+            else if (strNumber.Length == 3)
+            {
+                var hundred = Get1digit(strNumber[0]);
+                if (number % 100 == 0)
+                {
+                    return ($"{hundred}ร้อย");
+                }
+                else
+                {
+                    return ($"{hundred}ร้อย{GetReadWordOfNumber(number % 100)}");
+                }
+            }
+
+            else if (strNumber.Length == 2)
+            {
                 if (strNumber[0] == '1')
                 {
                     result = "สิบ";
@@ -24,13 +88,18 @@ namespace homework11
                     var actual = Get1digit(strNumber[0]);
                     result = ($"{actual}สิบ");
                 }
-                if (strNumber[1]=='0')
+
+                if (strNumber[1] == '0')
                 {
                     return result;
                 }
+                else if (strNumber[1] == '1')
+                {
+                    return ($"{result}เอ็ด");
+                }
                 var get1Digit = Get1digit(strNumber[1]);
                 return ($"{result}{get1Digit}");
-            }                     
+            }
             else
             {
                 return Get1digit(strNumber[0]);
