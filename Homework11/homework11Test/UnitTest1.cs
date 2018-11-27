fd475f61292b9c598361f2c797c1fddfdde70af0
powerofwind <powerofwind10@gmail.com>
2018-11-27 23:17:40 +0700

    implement code[0] in if (text.Length == 2)

--- Homework11/homework11Test/UnitTest1.cs
+++ Homework11/homework11Test/UnitTest1.cs
@@ -6,10 +6,10 @@ namespace homework11Test
 {
     public class UnitTest1
     {
-        [Theory (DisplayName="เลขหนึ่งหลัก")]
-        [InlineData (1,"หนึ่ง")]
-        [InlineData (0,"ศูนย์")]
-        [InlineData (8,"แปด")]
+        [Theory(DisplayName = "เลขหนึ่งหลัก")]
+        [InlineData(1, "หนึ่ง")]
+        [InlineData(0, "ศูนย์")]
+        [InlineData(8, "แปด")]
 
 
         public void onedigit(int number, string expected)
@@ -19,10 +19,17 @@ namespace homework11Test
             Assert.Equal(expected, result);
         }
 
-        [Theory (DisplayName="เลขสองหลักลงท้ายที่สิบ")]
-        [InlineData (10,"สิบ")]
-        [InlineData (20,"ยี่สิบ")]
-        [InlineData (80,"แปดสิบ")]
+        [Theory(DisplayName = "เลขสองหลักลงท้ายที่สิบ")]
+        [InlineData(10, "สิบ")]
+        [InlineData(20, "ยี่สิบ")]
+        [InlineData(30, "สามสิบ")]
+        [InlineData(40, "สี่สิบ")]
+        [InlineData(50, "ห้าสิบ")]
+        [InlineData(60, "หกสิบ")]
+        [InlineData(70, "เจ็ดสิบ")]
+        [InlineData(80, "แปดสิบ")]
+        [InlineData(90, "เก้าสิบ")]
+
 
 
         public void twodigit(int number, string expected)
@@ -31,5 +38,7 @@ namespace homework11Test
             var result = sut.GetReadWordOfNumber(number);
             Assert.Equal(expected, result);
         }
+
+
     }
 }
--- Homework11/homework11class/Logic.cs
+++ Homework11/homework11class/Logic.cs
@@ -5,93 +5,113 @@ namespace homework11class
     public class Logic : IHomework11
     {
         public string GetReadWordOfNumber(int number)
-        {   //12
+        {
             var text = number.ToString();
             if (text.Length == 2)
             {
-               if (text == "90")
+                if (text[0] == '1')
                 {
-                    return "เก้าสิบ";
+                    return "สิบ";
                 }
-                if (text == "80")
-                {
-                    return "แปดสิบ";
-                }
-                if (text == "70")
-                {
-                    return "เจ็ดสิบ";
-                }
-                if (text == "60")
-                {
-                    return "หกสิบ";
-                }
-                if (text == "50")
-                {
-                    return "ห้าสิบ";
-                }
-                if (text == "40")
-                {
-                    return "สี่สิบ";
-                }
-                if (text == "30")
-                {
-                    return "สามสิบ";
-                }
-                if (text == "20")
+                else if (text[0] == '2')
                 {
                     return "ยี่สิบ";
                 }
-                if (text == "10")
+                else if (text[0] >= '3' && text[0] > '0')
                 {
-                    return "สิบ";
-                } 
+                    var onedigit = Getonedigit(text[0]);
+                    return ($"{onedigit}สิบ");
+
+                }
+                // if (text == "90")
+                // {
+                //     return "เก้าสิบ";
+                // }
+                // else if (text == "80")
+                // {
+                //     return "แปดสิบ";
+                // }
+                // else if (text == "70")
+                // {
+                //     return "เจ็ดสิบ";
+                // }
+                // else if (text == "60")
+                // {
+                //     return "หกสิบ";
+                // }
+                // else if (text == "50")
+                // {
+                //     return "ห้าสิบ";
+                // }
+                // else if (text == "40")
+                // {
+                //     return "สี่สิบ";
+                // }
+                // else if (text == "30")
+                // {
+                //     return "สามสิบ";
+                // }
+                // else if (text == "20")
+                // {
+                //     return "ยี่สิบ";
+                // }
+                // else if (text == "10")
+                // {
+                //     return "สิบ";
+                // }
             }
             else
             {
-                if (text == "9")
-                {
-                    return "เก้า";
-                }
-                if (text == "8")
-                {
-                    return "แปด";
-                }
-                if (text == "7")
-                {
-                    return "เจ็ด";
-                }
-                if (text == "6")
-                {
-                    return "หก";
-                }
-                if (text == "5")
-                {
-                    return "ห้า";
-                }
-                if (text == "4")
-                {
-                    return "สี่";
-                }
-                if (text == "3")
-                {
-                    return "สาม";
-                }
-                if (text == "2")
-                {
-                    return "สอง";
-                }
-                if (text == "1")
-                {
-                    return "หนึ่ง";
-                }
-                if (text == "0")
-                {
-                    return "ศูนย์";
-                }
+                return Getonedigit(text[0]);
             }
 
             return "";
+        }
+
+        public string Getonedigit(char onedigit)
+        {
+            if (onedigit == '9')
+            {
+                return "เก้า";
+            }
+            else if (onedigit == '8')
+            {
+                return "แปด";
+            }
+            else if (onedigit == '7')
+            {
+                return "เจ็ด";
+            }
+            else if (onedigit == '6')
+            {
+                return "หก";
+            }
+            else if (onedigit == '5')
+            {
+                return "ห้า";
+            }
+            else if (onedigit == '4')
+            {
+                return "สี่";
+            }
+            else if (onedigit == '3')
+            {
+                return "สาม";
+            }
+            else if (onedigit == '2')
+            {
+                return "สอง";
+            }
+            else if (onedigit == '1')
+            {
+                return "หนึ่ง";
+            }
+            else
+            {
+                return "ศูนย์";
+            }
 
         }
+
     }
 }
