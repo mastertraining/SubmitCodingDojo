45f309c2660c3e83037774989884c71086d711f6
aphaphon <pui.aphaphon@gmail.com>
2018-11-27 21:47:00 +0700

    add more hard code implement completed

--- HomeWork 11/homework11.Test/GetReadWordOfNumberTest.cs
+++ HomeWork 11/homework11.Test/GetReadWordOfNumberTest.cs
@@ -5,7 +5,7 @@ namespace homework11.Test
 {
     public class GetReadWordOfNumberTest
     {
-        [Theory]
+        [Theory(DisplayName="ตัวเลขหลักหน่วย")]
         [InlineData(0,"ศูนย์")]
         [InlineData(1,"หนึ่ง")]
         [InlineData(2,"สอง")]
@@ -17,6 +17,23 @@ namespace homework11.Test
         [InlineData(8,"แปด")]
         [InlineData(9,"เก้า")]
         public void CheckGetReadWordOfNumber1Digit(int inPutNumber,string expected)
+        {
+            var sut = new GetReadWordOfNumberLogic();
+            var result = sut.GetReadWordOfNumber(inPutNumber);
+            Assert.Equal(result,expected);
+        }
+       
+        [Theory(DisplayName="ตัวเลขหลักสิบ")]
+        [InlineData(10,"สิบ")]
+        [InlineData(20,"ยี่สิบ")]
+        [InlineData(30,"สามสิบ")]
+        [InlineData(40,"สี่สิบ")]
+        [InlineData(50,"ห้าสิบ")]
+        [InlineData(60,"หกสิบ")]
+        [InlineData(70,"เจ็ดสิบ")]
+        [InlineData(80,"แปดสิบ")]
+        [InlineData(90,"เก้าสิบ")]
+        public void CheckGetReadWordOfNumber2Digit(int inPutNumber,string expected)
         {
             var sut = new GetReadWordOfNumberLogic();
             var result = sut.GetReadWordOfNumber(inPutNumber);
--- HomeWork 11/homework11/GetReadWordOfNumberLogic.cs
+++ HomeWork 11/homework11/GetReadWordOfNumberLogic.cs
@@ -6,52 +6,90 @@ namespace homework11
     public class GetReadWordOfNumberLogic : IHomework11
     {
         public string GetReadWordOfNumber(int number)
-        {           
-            if (number == 0)
+        {
+            var strNumber = number.ToString();
+            if (strNumber.Length == 2)
             {
-                return "ศูนย์";
-            }
-            else if (number == 1)
-            {
-                return "หนึ่ง";
-            }
-            else if (number == 2)
-            {
-                return "สอง";
-            }
-            else if (number == 3)
-            {
-                return "สาม";
-            }
-            else if (number == 4)
-            {
-                return "สี่";
-            }
-            else if (number == 5)
-            {
-                return "ห้า";
-            }
-            else if (number == 6)
-            {
-                return "หก";
-            }
-            else if (number == 7)
-            {
-                return "เจ็ด";
-            }
-            else if (number == 8)
-            {
-                return "แปด";
-            }
-            else if (number == 9)
-            {
-                return "เก้า";
+                if (strNumber == "10")
+                {
+                    return "สิบ";
+                }
+                else if (strNumber == "20")
+                {
+                    return "ยี่สิบ";
+                }
+                else if (strNumber == "30")
+                {
+                    return "สามสิบ";
+                }
+                else if (strNumber == "40")
+                {
+                    return "สี่สิบ";
+                }
+                else if (strNumber == "50")
+                {
+                    return "ห้าสิบ";
+                }
+                else if (strNumber == "60")
+                {
+                    return "หกสิบ";
+                }
+                else if (strNumber == "70")
+                {
+                    return "เจ็ดสิบ";
+                }
+                else if (strNumber == "80")
+                {
+                    return "แปดสิบ";
+                }
+                else
+                {
+                    return "เก้าสิบ";
+                }
             }
             else
             {
-                return "please in put a number only!";
+                if (strNumber == "0")
+                {
+                    return "ศูนย์";
+                }
+                else if (strNumber == "1")
+                {
+                    return "หนึ่ง";
+                }
+                else if (strNumber == "2")
+                {
+                    return "สอง";
+                }
+                else if (strNumber == "3")
+                {
+                    return "สาม";
+                }
+                else if (strNumber == "4")
+                {
+                    return "สี่";
+                }
+                else if (strNumber == "5")
+                {
+                    return "ห้า";
+                }
+                else if (strNumber == "6")
+                {
+                    return "หก";
+                }
+                else if (strNumber == "7")
+                {
+                    return "เจ็ด";
+                }
+                else if (strNumber == "8")
+                {
+                    return "แปด";
+                }
+                else
+                {
+                    return "เก้า";
+                }
             }
-            
         }
     }
 }
