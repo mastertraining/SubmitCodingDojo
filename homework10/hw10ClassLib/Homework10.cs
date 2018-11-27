ffb8dd61fc16eb2ee8a65cc6949abf0f5baa9a5f
woraput <woraput@kkumail.com>
2018-11-28 03:59:13 +0700

    start imprement hw10

--- homework10/hw10ClassLib/Homework10.cs
+++ homework10/hw10ClassLib/Homework10.cs
@@ -1,23 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace hw10ClassLib
 {
     public class Homework10 : IHomework10
     {
+        private List<IProduct> _cart = new List<IProduct>();
+        private List<IProduct> _products = new List<IProduct>();
+        private string _path = @"..\hw10Console\product.csv";
         public void AddProductToCart(IProduct product)
         {
-            throw new NotImplementedException();
+            _cart.Add(product);
         }
 
         public IEnumerable<IProduct> GetAllProducts()
         {
-            throw new NotImplementedException();
+            using (var reader = new StreamReader(_path))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var read = reader.ReadLine();
+                    var splitLine = read.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+                    foreach (var item in splitLine)
+                    {
+                        var values = item.Split(',');
+                        Double.TryParse(values[2], out double priceDouble);
+                        _products.Add(new Product
+                        {
+                            SKU = values[0],
+                            Name = values[1],
+                            Price = priceDouble,
+                        });
+                    }
+                }
+            }
+
+            return _products;
         }
 
         public IEnumerable<IProduct> GetProductsInCart()
         {
-            throw new NotImplementedException();
+            return _cart;
         }
 
         public string LoadSavedCart()
--- homework10/hw10Console/Program.cs
+++ homework10/hw10Console/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using hw10ClassLib;
 
 namespace hw10Console
 {
@@ -6,7 +8,48 @@ namespace hw10Console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var call = new Homework10();
+            var amount = 0.00;
+            var input = "";
+            var getAllProduct = call.GetAllProducts().ToList();
+
+            while (true)
+            {
+                var productsInCart = call.GetProductsInCart().ToList();
+
+                Console.WriteLine();
+                Console.WriteLine("Products in your cart are");
+
+                if (productsInCart.Capacity == 0)
+                {
+                    Console.WriteLine("none");
+                }
+                else
+                {
+                    var no = 1;
+
+                    foreach (var item in productsInCart)
+                    {
+                        Console.WriteLine($"{no}.{item.SKU},{item.Name},{item.Price,2:N}");
+                        no++;
+                    }
+                }
+
+                Console.WriteLine("---");
+                Console.WriteLine($"Total amount: {amount,2:N} baht");
+                Console.Write("Please input a product key:");
+                input = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(input))
+                {
+                    break;
+                }
+
+                var addToCart = getAllProduct.Find(it => it.SKU == input);
+
+                call.AddProductToCart(addToCart);
+                amount += addToCart.Price;
+            }
         }
     }
 }
