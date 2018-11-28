382b33eb8f00b467b65e55ee7ba7c3afaf4b4ecf
Earn <earndarkness@perfenterprise.com>
2018-11-28 19:30:48 +0700

    Add console project

---
+++ HomeWork12/homework12.console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using homework12.lib;
+
+namespace homework12.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write($"Hello Welcome to FirstCharactor analysis (HomeWork 12){Environment.NewLine}Please input string: ");
+            var text = Console.ReadLine();
+            var textComparison = new Homework12();
+            Console.WriteLine($"First duplicate charactor is: {textComparison.FirstDuplicateCharactor(text)}");
+            Console.WriteLine($"First not duplicate charactor is: {textComparison.FirstNotDuplicateCharactor(text)}");
+        }
+    }
+}
