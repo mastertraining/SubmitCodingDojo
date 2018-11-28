eecd6dcb47d9b0a1d6a556fd9461195e3fd64daa
woraput <woraput@kkumail.com>
2018-11-28 07:40:59 +0700

    can incread follow count  remaining sum of amount

--- homework10/hw10Console/Program.cs
+++ homework10/hw10Console/Program.cs
@@ -11,15 +11,15 @@ namespace hw10Console
 
             var call = new Homework10();
             var amount = 0.00;
-            var input = "";
+            var input = " ";
             var getAllProduct = call.GetAllProducts().ToList();
-            var statusSave = false;
             var massage = "Current state had been saved!";
 
             while (true)
             {
                 var productsInCart = call.GetProductsInCart().ToList();
-
+                var products = productsInCart.GroupBy(it => it.SKU);
+              
                 Console.WriteLine("Products in your cart are");
 
                 if (productsInCart.Capacity == 0)
@@ -29,40 +29,50 @@ namespace hw10Console
                 else
                 {
                     var no = 1;
-
-                    foreach (var item in productsInCart)
+                    foreach (var item in products)
                     {
-                        Console.WriteLine($"{no}.{item.SKU},{item.Name},{item.Price,2:N}");
+                        Console.WriteLine($"{no}.{item.First().Name,-29:G} ({item.Count()}) {item.First().Price,2:N}");
                         no++;
                     }
                 }
 
                 Console.WriteLine("---");
                 Console.WriteLine($"Total amount: {amount,2:N} baht");
-                Console.Write("Please input a product key:");
+                Console.Write("Please input a product key: ");
                 input = Console.ReadLine();
 
                 if (string.IsNullOrEmpty(input))
                 {
                     break;
                 }
-                if (input == "save")
+                else if (input == "save")
                 {
                     call.SaveCurrentState();
-                    statusSave = true;
-                }
-
-                var addToCart = getAllProduct.Find(it => it.SKU == input);
-
-                call.AddProductToCart(addToCart);
-                amount += addToCart.Price;
-                
-                if (statusSave)
-                {
                     Console.WriteLine(massage);
+                    amount = amount;
                 }
-                else Console.WriteLine();
+                else if (input.Contains(','))
+                {
+                    var split = input.Split(',').ToList();
+                    var value = int.TryParse(split[1], out int test) ? int.Parse(split[1]) : 1;
 
+                    var addToCart = getAllProduct.Find(it => it.SKU == split[0]);
+
+                    for (int i = 0; i < value; i++)
+                    {
+                        call.AddProductToCart(addToCart);
+                    }
+                    amount += addToCart.Price;
+                    Console.WriteLine();
+                }
+                else
+                {
+                    var addToCart = getAllProduct.Find(it => it.SKU == input);
+
+                    call.AddProductToCart(addToCart);
+                    amount += addToCart.Price;
+                    Console.WriteLine();
+                }
             }
         }
     }
