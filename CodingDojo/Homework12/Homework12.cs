034105b01d2af7baa024b394e4b63429cac0ce3c
Anawat <anawat@perfenterprise.com>
2018-11-28 17:58:46 +0700

    HW12: Test interface

---
+++ CodingDojo/Homework12/Homework12.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework12
+{
+    public class Homework12 : IHomework12
+    {
+        public char FirstDuplicateCharactor(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public char FirstNotDuplicateCharactor(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CodingDojo/Homework12/IHomework12.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Homework12
+{
+    public interface IHomework12
+    {
+        char FirstDuplicateCharactor(string text);
+        char FirstNotDuplicateCharactor(string text);
+    }
+}
