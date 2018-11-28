072b46cdf9a3c02f12bf14f0ad403bc7c708be1d
Earn <earndarkness@perfenterprise.com>
2018-11-28 19:25:32 +0700

    Refactor code
    Signed-off-by: Earn <earndarkness@perfenterprise.com>

--- HomeWork12/homework12.lib/Homework12.cs
+++ HomeWork12/homework12.lib/Homework12.cs
@@ -1,42 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace homework12.lib
 {
     public class Homework12 : IHomework12
     {
-        public char FirstDuplicateCharactor(string text)
+        public char FirstDuplicateCharactor(string text) => IsFindFirstUnique(text, false);
+
+        public char FirstNotDuplicateCharactor(string text) => IsFindFirstUnique(text, true);
+
+        private char IsFindFirstUnique(string text, bool isFindFirstUnique)
         {
             const char DefaultResult = '-';
             var isTextValid = !string.IsNullOrWhiteSpace(text);
             if (!isTextValid) return DefaultResult;
 
             var charArray = text.ToCharArray().ToList();
-            for (int i = 0; i < charArray.Count; i++)
+            foreach (var item in charArray)
             {
-                var copyCharArray = text.ToCharArray().ToList();
-                copyCharArray.RemoveAt(i);
-
-                var isDuplicated = copyCharArray.Any(it => it.Equals(charArray[i]));
-                if (isDuplicated) return charArray[i];
-            }
-            return DefaultResult;
-        }
-
-        public char FirstNotDuplicateCharactor(string text)
-        {
-            const char DefaultResult = '-';
-            var isTextValid = !string.IsNullOrWhiteSpace(text);
-            if (!isTextValid) return DefaultResult;
-
-            var charArray = text.ToCharArray().ToList();
-            for (int i = 0; i < charArray.Count; i++)
-            {
-                var copyCharArray = text.ToCharArray().ToList();
-                copyCharArray.RemoveAt(i);
-
-                var isUnique = !copyCharArray.Any(it => it.Equals(charArray[i]));
-                if (isUnique) return charArray[i];
+                var comparisonCharArry = new List<char>(charArray);
+                comparisonCharArry.Remove(item);
+                var isMatchedCondition = isFindFirstUnique ?
+                                         !comparisonCharArry.Any(it => it.Equals(item)) : comparisonCharArry.Any(it => it.Equals(item));
+                if (isMatchedCondition) return item;
             }
             return DefaultResult;
         }
