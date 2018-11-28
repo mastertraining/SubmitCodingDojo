5cffa5c5e42db2a616c8db4990752315a311654e
Anawat <anawat@perfenterprise.com>
2018-11-28 09:13:05 +0700

    fix bug from rename namespace

--- CodingDojo/HomeworkXUnit/Homework01to10/Homework06UnitTest.cs
+++ CodingDojo/HomeworkXUnit/Homework01to10/Homework06UnitTest.cs
@@ -45,7 +45,7 @@ namespace HomeworkXUnit
                 }
                 IHW.SaveCurrentState();
                 currentState.Should().Be(expected);
-                var HW06 = new Homework06();
+                var HW06 = new Homework06.Homework06();
                 var result = HW06.LoadState();
                 result.Should().Be(expected);
             }
@@ -69,7 +69,7 @@ namespace HomeworkXUnit
                 }
                 result.Should().Be(expected);
                 IHW.SaveCurrentState();
-                var HW06 = new Homework06();
+                var HW06 = new Homework06.Homework06();
                 var anotherResult = HW06.LoadState();
                 anotherResult.Should().Be(expected);
             }
