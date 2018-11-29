8665e67878bf276c8b08213915c60eea6d82aab9
woraput <woraput@kkumail.com>
2018-11-29 16:54:51 +0700

    setup hw12

---
+++ homework12/hw12ClassLib/Homework12.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace hw12ClassLib
+{
+    public class Homework12 : IHomework12
+    {
+        public char FirstDuplicateCharactor(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public char FirstNotDuplicateCharactor(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
