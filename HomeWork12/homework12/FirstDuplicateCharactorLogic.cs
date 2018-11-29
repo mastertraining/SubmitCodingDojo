5d2bc9c81a0be18c478c129cfa6270cca30cc441
aphaphon <pui.aphaphon@gmail.com>
2018-11-29 22:12:58 +0700

    first try to Refactor completed

--- HomeWork12/homework12/FirstDuplicateCharactorLogic.cs
+++ HomeWork12/homework12/FirstDuplicateCharactorLogic.cs
@@ -7,15 +7,15 @@ namespace homework12
     {
         public char FirstDuplicateCharactor(string text)
         {
-            var firstDuplicateChar = text.ToCharArray().GroupBy(it => it).FirstOrDefault(it => it.Count() > 1);
-            var result = firstDuplicateChar == null ? '-' : firstDuplicateChar.Key;
+            var qry = text.GroupBy(it => it).FirstOrDefault(it => it.Count() > 1);
+            var result = qry == null ? '-' : qry.Key;
             return result;
         }
 
         public char FirstNotDuplicateCharactor(string text)
         {
-            var FirstNotDuplicateChar = text.ToCharArray().GroupBy(it => it).FirstOrDefault(it => it.Count() == 1);
-            var result = FirstNotDuplicateChar == null ? '-' : FirstNotDuplicateChar.Key;
+            var qry = text.GroupBy(it => it).FirstOrDefault(it => it.Count() == 1);
+            var result = qry == null ? '-' : qry.Key;
             return result;
         }
     }
