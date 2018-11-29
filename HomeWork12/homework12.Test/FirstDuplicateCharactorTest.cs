5b02e7874f1917d597a0412049bd38a1d4f35264
aphaphon <pui.aphaphon@gmail.com>
2018-11-29 19:12:39 +0700

    add to test

---
+++ HomeWork12/homework12.Test/FirstDuplicateCharactorTest.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+
+namespace homework12.Test
+{
+    public class FirstDuplicateCharactorTest
+    {
+        [Theory]
+        [InlineData("ZXCVBNMASDFGHJ",'-')]
+        public void CheckFirstDuplicateCharactor(string inputString, char expected)
+        {
+            var sut = new FirstDuplicateCharactorLogic();
+            var result = sut.FirstDuplicateCharactor(inputString);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("ZXCVBNMASDFGHJ",'Z')]
+        public void CheckFirstNotDuplicateCharactorr(string inputString, char expected)
+        {
+            var sut = new FirstDuplicateCharactorLogic();
+            var result = sut.FirstNotDuplicateCharactor(inputString);
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ HomeWork12/homework12/FirstDuplicateCharactorLogic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace homework12
+{
+    public class FirstDuplicateCharactorLogic : IHomework12
+    {
+        public char FirstDuplicateCharactor(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public char FirstNotDuplicateCharactor(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
