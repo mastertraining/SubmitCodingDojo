a4d8ddc42ee1245564652b0ed508799a0cf4ae68
powerofwind <powerofwind10@gmail.com>
2018-11-30 11:26:58 +0700

    FirstDuplicateCharactor complete

---
+++ Homework12/Homework12classlib/Logic.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Homework12classlib
+{
+    public class Logic : IHomework12
+    {
+        public char FirstDuplicateCharactor(string text)
+        {
+            var returnchar = text.ToCharArray();
+            for (int i = 0; i < returnchar.Length; i++)
+            {
+                for (int j = i + 1; j < returnchar.Length; j++)
+                {
+                    if (returnchar[i] == returnchar[j])
+                    {
+                        return returnchar[i];
+                    }
+                }
+            }
+             return '-';
+        }
+
+        public char FirstNotDuplicateCharactor(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
