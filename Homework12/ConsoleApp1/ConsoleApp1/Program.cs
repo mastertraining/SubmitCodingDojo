a8e55c120f936f278cbc2180f7cbd9c9a6df6bf7
thaninpong <thaninpong@perfenterprise.com>
2018-11-30 13:02:10 +0700

    HW12

---
+++ Homework12/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using HomeWork12.lib;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = new Work();
+            var result = data.FirstDuplicateCharactor("ZXCZXCVBNMASDFGHJVBNMASZXCVBNMASDFGHJDFGHJ");
+            var result2 = data.FirstNotDuplicateCharactor("ZXCZXCVBNMASDFGHJVBNMASZXCVBNMASDFGHJDFGHJ");
+            Console.WriteLine(result);
+            Console.WriteLine(result2);
+        }
+    }
+}
---
+++ Homework12/ConsoleApp1/HomeWork12.lib/IHomework12.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork12.lib
+{
+    public interface IHomework12
+    {
+        char FirstDuplicateCharactor(string text);
+        char FirstNotDuplicateCharactor(string text);
+    }
+}
