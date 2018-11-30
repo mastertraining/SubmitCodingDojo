1e2acd548a9c5a43665e9f0904167015ceb3f4ef
SorrY <so.sikarin@perfenterprise.com>
2018-11-30 10:01:18 +0700

    1212

--- Homework12/Homework.Lib/Charactor.cs
+++ Homework12/Homework.Lib/Charactor.cs
@@ -1,19 +1,13 @@
 ﻿using System;
 using System.Linq;
 
-namespace Homework.Lib
-{
-    public class Charactor : IHomework12
-    {
-        public char FirstDuplicateCharactor(string text)
-        {
-            var cha = text.ToCharArray();
-            for (int i = 0; i < cha.Length - 1; i++)
-            {
-                for (int j = 0; j < cha.Length - 1; j++)
-                {
-                    if (cha[i] == cha[j + 1])
-                    {
+namespace Homework.Lib {
+    public class Charactor : IHomework12 {
+        public char FirstDuplicateCharactor (string text) {
+            var cha = text.ToCharArray ();
+            for (int i = 0; i < cha.Length - 1; i++) {
+                for (int j = 0; j < cha.Length - 1; j++) {
+                    if (cha[i] == cha[j + 1]) {
                         return cha[i];
                     }
                 }
@@ -21,9 +15,11 @@ namespace Homework.Lib
             return '-';
         }
 
-        public char FirstNotDuplicateCharactor(string text)
-        {
-            var cha = text.ToCharArray();
+        public char FirstNotDuplicateCharactor (string text) {
+            var cha = text.ToCharArray ().GroupBy (it => it).FirstOrDefault (it => it.Count () == 1);
+            if(cha != null){
+                return cha.Key;
+            }
             return '-';
         }
     }
