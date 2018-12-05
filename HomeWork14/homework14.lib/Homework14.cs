8a23f9bb70ff7b56c570bb6fc239958c74ef4bae
Earn <earndarkness@perfenterprise.com>
2018-12-05 16:33:42 +0700

    Implement fully game calculate in Homework14
    Signed-off-by: Earn <earndarkness@perfenterprise.com>

--- HomeWork14/homework14.lib/Homework14.cs
+++ HomeWork14/homework14.lib/Homework14.cs
@@ -6,15 +6,21 @@ namespace homework14.lib
     public class Homework14 : IHomework14
     {
         private int player1_distance;
+        private int player1_distanceStack;
+        private int player1_distanceStackCount;
         private int player2_distance;
+        private int player2_distanceStack;
+        private int player2_distanceStackCount;
 
         private string RenderDistance
         {
             get
             {
                 var display = new StringBuilder();
-                display.AppendLine($"Koo ({player1_distance}): {new string('*', player1_distance)}");
-                display.Append($"Kee ({player2_distance}): {new string('*', player2_distance)}");
+                display.AppendLine($"Koo ({player1_distance.ToString("00")}): {new string('*', player1_distance)}");
+                display.Append($"Kee ({player2_distance.ToString("00")}): {new string('*', player2_distance)}");
+                if (player1_distance == 0) display.Append($"{Environment.NewLine}The Winner is MR.Koo");
+                if (player2_distance == 0) display.Append($"{Environment.NewLine}The Winner is MR.Kee");
                 return display.ToString();
             }
         }
@@ -26,8 +32,29 @@ namespace homework14.lib
 
         public string GetGameResult(int number)
         {
-            if (number % 2 == 0) player1_distance--;
-            else player2_distance--;
+            if (number % 2 == 0)
+            {
+                player1_distance -= 1 + player2_distanceStackCount;
+                player1_distanceStack++;
+                player2_distanceStack = 0;
+            }
+            else
+            {
+                player2_distance -= 1 + player1_distanceStackCount;
+                player1_distanceStack = 0;
+                player2_distanceStack++;
+            }
+
+            if (player2_distanceStack == 3)
+            {
+                player2_distanceStackCount++;
+                player2_distanceStack = 0;
+            }
+            if (player1_distanceStack == 3)
+            {
+                player1_distanceStackCount++;
+                player1_distanceStack = 0;
+            }
             return RenderDistance;
         }
 
@@ -36,6 +63,10 @@ namespace homework14.lib
             const int DefaultDistance = 20;
             player1_distance = DefaultDistance;
             player2_distance = DefaultDistance;
+            player1_distanceStack = 0;
+            player1_distanceStackCount = 0;
+            player2_distanceStack = 0;
+            player2_distanceStackCount = 0;
         }
     }
 }
