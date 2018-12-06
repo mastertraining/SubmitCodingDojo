b5f3293d93fa56d1c638a3d5b89b163464d1bebd
powerofwind <powerofwind10@gmail.com>
2018-12-07 01:58:06 +0700

    test logic complete

--- Homework14/Homework14/Program.cs
+++ Homework14/Homework14/Program.cs
@@ -8,46 +8,23 @@ namespace Homework14
     {
         static void Main(string[] args)
         {
-            Console.Write("Please input a number: ");
-            var input = int.Parse(Console.ReadLine());
-            // var sn = new Logic();
-            // var result = sn.GetGameResult(input);
-            // Console.WriteLine(result);
-            int _speedkoo = 1;
-            int _speedkee = 1;
-            int _numdistancekoo = 20;
-            int _numdistancekee = 20;
-            int _countkoo = 0;
-            int _countkee = 0;
+            var sn = new Logic();
 
-            if (input % 2 == 0)
+            var replay = "yes";
+            while (replay == "yes")
             {
-                _countkee++;
-                _speedkoo += _countkoo / 3;
-                _numdistancekoo -= _speedkoo;
-                _countkoo = 0;
-
+                // if (replay == "yes")
+                // {
+                //     Console.WriteLine(sn.StartGame);
+                //     Console.Write("Please input a number: ");
+                // }
+                
+                // Console.WriteLine(sn.StartGame);
+                 Console.Write("Please input a number: ");
+                var input = int.Parse(Console.ReadLine());
+                var result = sn.GetGameResult(input);
+                Console.WriteLine(result);
             }
-            else
-            {
-                _countkoo++;
-                _speedkee += _countkee / 3;
-                _numdistancekee -= _speedkee;
-                _countkee = 0;
-            }
-
-            var sb = new StringBuilder();
-            // addStr.Append(new string(' ', input - i)).Append("/").Append("|").AppendLine();
-            sb.Append("Koo ").Append($"({_numdistancekoo}):").AppendLine()
-              .Append("Kee ").Append($"({_numdistancekee}):");
-            //return sb;
-
-            Console.WriteLine(sb);
-            
-
-
-
-
 
         }
     }
--- Homework14/HomeworkclassLib/Logic.cs
+++ Homework14/HomeworkclassLib/Logic.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace HomeworkclassLib
 {
@@ -6,16 +7,52 @@ namespace HomeworkclassLib
     {
         private int _speedkoo = 1;
         private int _speedkee = 1;
-        private int _distancekoo = 20;
-        private int _distancekee = 20;
+        private int _numdistancekoo = 20;
+        private int _numdistancekee = 20;
         private int _countkoo = 0;
         private int _countkee = 0;
 
-        public string GetGameResult(int number)
+        private string startGame;
+
+        public string StartGame
         {
-            throw new NotImplementedException();
+            get
+            {
+                var displayKoo = new string('*', 20);
+                var displayKee = new string('*', 20);
+
+                var sb = new StringBuilder();
+                sb.Append("Koo ").Append($"({_numdistancekoo}): {displayKoo}").AppendLine()
+                  .Append("Kee ").Append($"({_numdistancekee}): {displayKee}");
+                return sb.ToString();
+            }
         }
 
+        public string GetGameResult(int number)
+        {
+            if (number % 2 == 0)
+            {
+                _countkee++;
+                _speedkoo += _countkoo / 3;
+                _numdistancekoo -= _speedkoo;
+                _countkoo = 0;
+            }
+            else
+            {
+                _countkoo++;
+                _speedkee += _countkee / 3;
+                _numdistancekee -= _speedkee;
+                _countkee = 0;
+            }
+
+            var displayKoo = new string('*', _numdistancekoo);
+            var displayKee = new string('*', _numdistancekee);
+
+            var sb = new StringBuilder();
+            sb.Append("Koo ").Append($"({_numdistancekoo}): {displayKoo}").AppendLine()
+              .Append("Kee ").Append($"({_numdistancekee}): {displayKee}");
+            return sb.ToString();
+        }
         public void SetupANewGame()
         {
             throw new NotImplementedException();
