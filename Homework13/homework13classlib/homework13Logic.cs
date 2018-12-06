8dc9f8ab54e71f3255448a7325464b9d1dcff3ee
powerofwind <powerofwind10@gmail.com>
2018-12-06 23:59:17 +0700

    SET UP PROJECT

--- Homework13/homework13classlib/homework13Logic.cs
+++ Homework13/homework13classlib/homework13Logic.cs
@@ -7,7 +7,34 @@ namespace homework13classlib
     {
         public int Homework13(IEnumerable<int> numbers)
         {
-            throw new NotImplementedException();
+            var numberschar = numbers.ToString().ToCharArray();
+            var count = 0;
+            if (numberschar[0] == 9 && numberschar[1] == 9)
+            {
+                // count ++;
+                return 1;
+            }
+            return 0;
+
+
+
+            // var count = 0;
+            // foreach (var numb in numbers)
+            // {
+            //     var StrNum = numb.ToString();
+            //     for (int i = 0; i < StrNum.Length; i++)
+            //     {
+            //         if (StrNum[i] == '9')
+            //         {
+            //             count++;
+            //         }
+            //     }
+
+            // }
+            // return count;
+
+
+            
         }
     }
 }
---
+++ Homework14/HomeworkclassLib/Logic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HomeworkclassLib
+{
+    public class Logic : IHomework14
+    {
+        public string GetGameResult(int number)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetupANewGame()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
