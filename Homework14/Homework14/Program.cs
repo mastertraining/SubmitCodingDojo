4ddda733ca7d55c5105f8e1610b2b38ed97f238b
powerofwind <powerofwind10@gmail.com>
2018-12-07 00:55:10 +0700

    create logic and displayresult(int)

--- Homework14/Homework14/Program.cs
+++ Homework14/Homework14/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+using HomeworkclassLib;
 
 namespace Homework14
 {
@@ -6,7 +8,47 @@ namespace Homework14
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Please input a number: ");
+            var input = int.Parse(Console.ReadLine());
+            // var sn = new Logic();
+            // var result = sn.GetGameResult(input);
+            // Console.WriteLine(result);
+            int _speedkoo = 1;
+            int _speedkee = 1;
+            int _numdistancekoo = 20;
+            int _numdistancekee = 20;
+            int _countkoo = 0;
+            int _countkee = 0;
+
+            if (input % 2 == 0)
+            {
+                _countkee++;
+                _speedkoo += _countkoo / 3;
+                _numdistancekoo -= _speedkoo;
+                _countkoo = 0;
+
+            }
+            else
+            {
+                _countkoo++;
+                _speedkee += _countkee / 3;
+                _numdistancekee -= _speedkee;
+                _countkee = 0;
+            }
+
+            var sb = new StringBuilder();
+            // addStr.Append(new string(' ', input - i)).Append("/").Append("|").AppendLine();
+            sb.Append("Koo ").Append($"({_numdistancekoo}):").AppendLine()
+              .Append("Kee ").Append($"({_numdistancekee}):");
+            //return sb;
+
+            Console.WriteLine(sb);
+            
+
+
+
+
+
         }
     }
 }
--- Homework14/HomeworkclassLib/Logic.cs
+++ Homework14/HomeworkclassLib/Logic.cs
@@ -4,6 +4,13 @@ namespace HomeworkclassLib
 {
     public class Logic : IHomework14
     {
+        private int _speedkoo = 1;
+        private int _speedkee = 1;
+        private int _distancekoo = 20;
+        private int _distancekee = 20;
+        private int _countkoo = 0;
+        private int _countkee = 0;
+
         public string GetGameResult(int number)
         {
             throw new NotImplementedException();
