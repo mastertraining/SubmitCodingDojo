dbf6833e84084c20d10300d6994914ce74f689b0
sirisak <sirisak022602020sunon@gmail.com>
2018-12-11 22:26:31 +0700

    homework12

---
+++ Homework12/Homework_classlib/Homework12.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Homework_classlib
+{
+    public class Homework12 : IHomework12
+    {
+        public char FirstDuplicateCharactor(string text)
+        {
+            var newText = text.GroupBy(it => it).FirstOrDefault(it => it.Count() > 1);
+            var result = newText != null ? newText.First() : '-';
+            return result;
+        }
+        
+        public char FirstNotDuplicateCharactor(string text)
+        {
+            var newText = text.GroupBy(it => it).FirstOrDefault(it => it.Count() == 1);
+            var result = newText != null ? newText.First() : '-';
+            return result; 
+        }
+    }
+}
